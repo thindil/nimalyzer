5bec9d82a80cf9079ea6ea92fa8716eb6f64aa0d
thindil <thindil@laeran.pl>
2023-01-12 05:04:32 +0000

    feat: started work on rule hasEntity
    FossilOrigin-Name: a9e6101c62f7f1a351cc21857754d9a9551af185bfbed882a1a22542fc48c570

---
+++ src/rules/hasentity.nim
@@ -0,0 +1,47 @@
+# Copyright © 2023 Bartek Jasicki <thindil@laeran.pl>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## The rule to check if the selected procedure has the selected entities, like
+## procedures, constants, etc.
+
+# Standard library imports
+import std/[logging, strutils]
+# External modules imports
+import compiler/[ast, renderer]
+
+const ruleName* = "hasentity"
+
+proc ruleCheck*(astTree: PNode; options: seq[string];
+    logger: ConsoleLogger): bool =
+  result = false
+  for node in astTree.items:
+    for child in node.items:
+      result = ruleCheck(astTree = child, options = options, logger = logger)
+      if result:
+        return
+    if node.kind != parseEnum[TNodeKind](s = options[0]):
+      continue
+    if $node[0] == options[1]:
+      return true
