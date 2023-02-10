234dc50fb7b298cd1f3954e6d4158f7e55e951c2
thindil <thindil@laeran.pl>
2023-02-10 08:12:33 +0000

    tests: added unit test for ruleCheck for hasDoc pragma
    FossilOrigin-Name: 5704c3c971d26d387a8ad4ac329fa62ca149ae03fffa2cc278292309cddfba31

--- tests/thasdoc/thasdoc.nim
+++ tests/thasdoc/thasdoc.nim
@@ -4,7 +4,9 @@ discard """
 """
 
 import std/logging
+import compiler/[idents, options, parser]
 import ../../src/rules/hasdoc
+import ../../src/rules
 
 let logger = newConsoleLogger(fmtStr = "[$time] - $levelname: ")
 addHandler(handler = logger)
@@ -12,3 +14,15 @@ setLogFilter(lvl = lvlInfo)
 
 assert not validateOptions(@["randomoption"])
 assert validateOptions(@[])
+
+let
+  nimCache = newIdentCache()
+  nimConfig = newConfigRef()
+nimConfig.options.excl(y = optHints)
+var
+  code = parseString("quit", nimCache, nimConfig)
+  ruleOptions = RuleOptions(parent: true, fileName: "test.nim", negation: false,
+      ruleType: check, options: @[], amount: 0)
+assert ruleCheck(code, ruleOptions) == -1
+code = parseString("## Doc", nimCache, nimConfig)
+assert ruleCheck(code, ruleOptions) == 1
