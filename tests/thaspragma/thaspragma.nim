19dfa625b24d17675e0ab28a03c0179d9991b530
thindil <thindil@laeran.pl>
2023-02-11 05:28:40 +0000

    tests: added unit test for ruleCheck for hasPragma rule
    FossilOrigin-Name: 622506cc9ceb911a12ac83e1b7f118e728d29b09661ea72d33608f5da0ee26bd

--- tests/thaspragma/thaspragma.nim
+++ tests/thaspragma/thaspragma.nim
@@ -4,6 +4,8 @@ discard """
 """
 
 import std/logging
+import compiler/[idents, options, parser]
+import ../../src/rules
 import ../../src/rules/haspragma
 
 let logger = newConsoleLogger(fmtStr = "[$time] - $levelname: ")
@@ -12,3 +14,15 @@ setLogFilter(lvl = lvlInfo)
 
 assert not validateOptions(@[])
 assert validateOptions(@["raises: [*"])
+
+let
+  nimCache = newIdentCache()
+  nimConfig = newConfigRef()
+nimConfig.options.excl(y = optHints)
+var
+  code = parseString("quit", nimCache, nimConfig)
+  ruleOptions = RuleOptions(parent: true, fileName: "test.nim", negation: false,
+      ruleType: check, options: @["raises: [*"], amount: 0)
+assert ruleCheck(code, ruleOptions) == 0
+code = parseString("proc MyProc() {.raises: [].} = discard", nimCache, nimConfig)
+assert ruleCheck(code, ruleOptions) == 1
