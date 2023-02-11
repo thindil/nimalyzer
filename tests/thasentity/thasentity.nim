9b3b2e8ee2cc52e07183b45a5ca6128ad746e1ef
thindil <thindil@laeran.pl>
2023-02-11 05:25:01 +0000

    tests: added unit test for ruleCheck for hasEntity pragma
    FossilOrigin-Name: e24e603f67b4c562f167ece123a72e5c671429f028a9f6075800026459f64616

--- tests/thasentity/thasentity.nim
+++ tests/thasentity/thasentity.nim
@@ -4,11 +4,25 @@ discard """
 """
 
 import std/logging
+import compiler/[idents, options, parser]
 import ../../src/rules/hasentity
+import ../../src/rules
 
 let logger = newConsoleLogger(fmtStr = "[$time] - $levelname: ")
 addHandler(handler = logger)
 setLogFilter(lvl = lvlInfo)
 
-assert not validateOptions(@[""])
+assert not validateOptions(@[])
 assert validateOptions(@["nkProcDef", "MyProc"])
+
+let
+  nimCache = newIdentCache()
+  nimConfig = newConfigRef()
+nimConfig.options.excl(y = optHints)
+var
+  code = parseString("quit", nimCache, nimConfig)
+  ruleOptions = RuleOptions(parent: true, fileName: "test.nim", negation: false,
+      ruleType: check, options: @["nkProcDef", "MyProc"], amount: 0)
+assert ruleCheck(code, ruleOptions) == -1
+code = parseString("proc MyProc() = discard", nimCache, nimConfig)
+assert ruleCheck(code, ruleOptions) == 1
