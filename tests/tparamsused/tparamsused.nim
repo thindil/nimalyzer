32e23a28c7a93b42e2ef3cea26d590b572796abc
thindil <thindil@laeran.pl>
2023-02-12 04:40:26 +0000

    tests: added unit test for ruleCheck for paramsUsed rule
    FossilOrigin-Name: 053c3932af6dfe9541368e8861f12c7be3d2dc7864dc50a5a5d496455b39515b

--- tests/tparamsused/tparamsused.nim
+++ tests/tparamsused/tparamsused.nim
@@ -4,6 +4,8 @@ discard """
 """
 
 import std/logging
+import compiler/[idents, options, parser]
+import ../../src/rules
 import ../../src/rules/paramsused
 
 let logger = newConsoleLogger(fmtStr = "[$time] - $levelname: ")
@@ -12,3 +14,15 @@ setLogFilter(lvl = lvlInfo)
 
 assert not validateOptions(@["randomoption"])
 assert validateOptions(@[])
+
+let
+  nimCache = newIdentCache()
+  nimConfig = newConfigRef()
+nimConfig.options.excl(y = optHints)
+var
+  code = parseString("proc MyProc(arg: int) = discard", nimCache, nimConfig)
+  ruleOptions = RuleOptions(parent: true, fileName: "test.nim", negation: false,
+      ruleType: check, options: @["raises: [*"], amount: 0)
+assert ruleCheck(code, ruleOptions) == -1
+code = parseString("proc MyProc(arg: int) = echo $arg", nimCache, nimConfig)
+assert ruleCheck(code, ruleOptions) == 1
