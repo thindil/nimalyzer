3cb5bc6e74be99b744c4c50a8c7101c4c6014a2f
thindil <thindil@laeran.pl>
2023-02-12 04:37:02 +0000

    tests: added unit test for ruleCheck for namedParams rule
    FossilOrigin-Name: 842dca307985778f3f7e57eaa274d46f4a235f9177d6c3996d9b3a080674d4be

--- tests/tnamedparams/tnamedparams.nim
+++ tests/tnamedparams/tnamedparams.nim
@@ -4,6 +4,8 @@ discard """
 """
 
 import std/logging
+import compiler/[idents, options, parser]
+import ../../src/rules
 import ../../src/rules/namedparams
 
 let logger = newConsoleLogger(fmtStr = "[$time] - $levelname: ")
@@ -12,3 +14,15 @@ setLogFilter(lvl = lvlInfo)
 
 assert not validateOptions(@["randomoption"])
 assert validateOptions(@[])
+
+let
+  nimCache = newIdentCache()
+  nimConfig = newConfigRef()
+nimConfig.options.excl(y = optHints)
+var
+  code = parseString("quit(QuitSuccess)", nimCache, nimConfig)
+  ruleOptions = RuleOptions(parent: true, fileName: "test.nim", negation: false,
+      ruleType: check, options: @[], amount: 0)
+assert ruleCheck(code, ruleOptions) == -1
+code = parseString("myProc(named = true)", nimCache, nimConfig)
+assert ruleCheck(code, ruleOptions) == 1
