9f6f15d85b6590a93759fc9d691f66425ed11d1a
thindil <thindil@laeran.pl>
2023-02-21 05:08:00 +0000

    tests: refactored tests for paramsUsed rule
    FossilOrigin-Name: c1d8f4159123c159b9516b329f1f66f2713deb123192e7682b8e1237b5664af1

--- tests/tparamsused/tparamsused.nim
+++ tests/tparamsused/tparamsused.nim
@@ -3,51 +3,15 @@ discard """
   outputsub: "randomoption"
 """
 
-import std/logging
 import compiler/[idents, options, parser]
 import ../../src/rules
 import ../../src/rules/paramsused
+import ../helpers.nim
 
-let logger = newConsoleLogger(fmtStr = "[$time] - $levelname: ")
-addHandler(handler = logger)
-setLogFilter(lvl = lvlInfo)
+const
+  validOptions = @[]
+  invalidOptions = @["randomoption"]
+  invalidNimCode = "proc MyProc(arg: int) = discard"
+  validNimCode = "proc MyProc(arg: int) = echo $arg"
 
-assert not validateOptions(@["randomoption"])
-assert validateOptions(@[])
-
-let
-  nimCache = newIdentCache()
-  nimConfig = newConfigRef()
-nimConfig.options.excl(y = optHints)
-let
-  invalidCode = parseString("proc MyProc(arg: int) = discard", nimCache, nimConfig)
-  validCode = parseString("proc MyProc(arg: int) = echo $arg", nimCache, nimConfig)
-var
-  ruleOptions = RuleOptions(parent: true, fileName: "test.nim", negation: false,
-      ruleType: check, options: @[], amount: 0)
-
-# check rule tests
-assert ruleCheck(invalidCode, ruleOptions) == 0
-assert ruleCheck(validCode, ruleOptions) == 1
-# negative check rule tests
-ruleOptions.negation = true
-assert ruleCheck(invalidCode, ruleOptions) == 1
-assert ruleCheck(validCode, ruleOptions) == 0
-# search rule tests
-ruleOptions.ruleType = search
-ruleOptions.negation = false
-assert ruleCheck(invalidCode, ruleOptions) == 0
-assert ruleCheck(validCode, ruleOptions) == 1
-# negative search rule tests
-ruleOptions.negation = true
-assert ruleCheck(invalidCode, ruleOptions) == 1
-assert ruleCheck(validCode, ruleOptions) == 0
-# count rule tests
-ruleOptions.ruleType = count
-ruleOptions.negation = false
-assert ruleCheck(invalidCode, ruleOptions) == 1
-assert ruleCheck(validCode, ruleOptions) == 1
-# negative count rule tests
-ruleOptions.negation = true
-assert ruleCheck(invalidCode, ruleOptions) == 1
-assert ruleCheck(validCode, ruleOptions) == 1
+runRuleTest()
