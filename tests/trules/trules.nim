29fac9b792bb4fb249ea5ca745048708362f999a
thindil <thindil@laeran.pl>
2023-02-27 05:10:51 +0000

    tests: added unit test for errorMessage procedure
    FossilOrigin-Name: e29970ef86b3250b51016cc29c75ab10ec07cc9714f02e6d38d75006ffc072d7

--- tests/trules/trules.nim
+++ tests/trules/trules.nim
@@ -15,3 +15,9 @@ message("Decrease value", resultValue)
 assert resultValue == -1
 message("Increase value", resultValue, lvlInfo, false)
 assert resultValue == 0
+
+try:
+  var i: int8 = int8.high
+  i.inc
+except:
+  assert errorMessage("Reason: ", getCurrentException()) == 0
