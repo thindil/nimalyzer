881362514a6654935b858f48d3403bad10131abd
thindil <thindil@laeran.pl>
2023-03-08 04:31:33 +0000

    feat: started work on rule varDeclared to check declarations of variables
    FossilOrigin-Name: aef1c1b43767538ab92240f4aa628dbfd75d75128968ce19534b69914dcc1f8a

---
+++ src/rules/vardeclared.nim
@@ -0,0 +1,157 @@
+# Copyright © 2023 Bartek Jasicki
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## The rule to check if the selected variable declaration (var, let and const)
+## has declared type and or value
+## The syntax in a configuration file is::
+##
+##   [ruleType] ?not? varDeclared [declarationType]
+##
+## * ruleType is the type of rule which will be executed. Proper values are:
+##   *check*, *search* and *count*. For more information about the types of
+##   rules, please refer to the program's documentation. Check type will raise
+##   an error if there is a declaration isn't in desired pattern. Search type
+##   will list all declarations with desired pattern and raise error if
+##   nothing was found. Count type will simply list the amount of declarations
+##   with the desired pattern.
+## * optional word *not* means negation for the rule. Adding word *not* will
+##   change to inform only about procedures without desired pattern.
+##   Probably useable only with search and count type of rule.
+## * declarationType is the desired type of variable's declaration to check.
+##   Possible values are: full - the declaration must have declared type and
+##   value for the variable, type - the declaration must have declared type for
+##   the variable, value - the declaration must have declared value for the
+##   variable.
+##
+## Disabling the rule
+## ------------------
+## It is possible to disable the rule for a selected part of the checked code
+## by using pragma *ruleOff: "varDeclared"* in the declaration from which the rule
+## should be disabled. For example, if the rule should be disabled for variable
+## `var a: int`, the full declaration of it should be::
+##
+##      var a: int {.ruleOff: "varDeclared".}
+##
+## To enable the rule again, the pragma *ruleOn: "varDeclared"* should be added in
+## the element which should be checked. For example, if the rule should be
+## re-enabled for variable `let b = 2`, the full declaration should be::
+##
+##      let b = 2 {.ruleOn: "varDeclared".}
+##
+## Examples
+## --------
+##
+## 1. Check if all declarations have set type and value for them::
+##
+##     check varDeclared full
+##
+## 2. Search for all declarations which don't set type for them::
+##
+##     search not varDeclared type
+
+# Standard library imports
+import std/[logging, strutils]
+# External modules imports
+import compiler/[ast, renderer]
+import contracts
+# Internal modules imports
+import ../rules
+
+const ruleName* = "vardeclared" ## The name of the rule used in a configuration file
+
+proc ruleCheck*(astTree: PNode; options: var RuleOptions) {.contractual,
+    raises: [], tags: [RootEffect].} =
+  ## Check recursively if all variables' declarations in Nim code follow
+  ## the selected pattern
+  ##
+  ## * astTree - The AST tree representation of the Nim code to check
+  ## * options - The rule options set by the user and the previous iterations
+  ##             of the procedure
+  ##
+  ## The amount of result how many declarations of variables follow the
+  ## selected pattern
+  require:
+    astTree != nil
+    options.fileName.len > 0
+  body:
+    let isParent = options.parent
+    if isParent:
+      options.parent = false
+    let messagePrefix = if getLogFilter() < lvlNotice:
+        ""
+      else:
+        options.fileName & ": "
+    for node in astTree.items:
+      # Check the node's children if rule is enabled
+      for child in node.items:
+        setRuleState(node = child, ruleName = ruleName,
+            oldState = options.enabled)
+      if options.enabled and node.kind in {nkVarSection, nkLetSection, nkConstSection}:
+        # Get the variable's name
+        let varName = try:
+              $node[0]
+            except KeyError, Exception:
+              ""
+        if varName.len == 0:
+          options.amount = errorMessage(
+              text = "Can't get the name of the variable.")
+          return
+      # Check the node's children with the rule
+      for child in node.items:
+        ruleCheck(astTree = child, options = options)
+    if isParent:
+      if options.amount < 0:
+        options.amount = 0
+      if options.ruleType == RuleTypes.count:
+        message(text = (if getLogFilter() <
+            lvlNotice: "D" else: options.fileName & ": d") &
+            "eclarations with" & (if options.negation: "out" else: "") &
+            " " & options.options[0] & " declaration found: " & $options.amount,
+                returnValue = options.amount,
+            level = lvlNotice)
+        options.amount = 1
+
+proc validateOptions*(options: seq[string]): bool {.contractual, raises: [],
+    tags: [RootEffect].} =
+  ## Validate the options entered from a configuration for the rule
+  ##
+  ## * options - the list of options entered from a configuration file
+  ##
+  ## Returns true if options are valid otherwise false.
+  body:
+    var tmpResult = 0
+    if options.len == 0:
+      message(text = "The rule varDeclared require type of declaration as the option, but nothing was supplied.",
+          returnValue = tmpResult, level = lvlFatal)
+      return false
+    elif options.len == 1 and options[0] notin ["full", "type", "value"]:
+      message(text = "The rule varDeclared require 'full', 'type' or 'value' as option, but suplied option was: '" &
+          options[0] & "'.", returnValue = tmpResult, level = lvlFatal)
+      return false
+    elif options.len > 1:
+      message(text = "The rule varDeclared require exactly one option, but more options suplied: '" &
+          options.join(", ") & "'.", returnValue = tmpResult, level = lvlFatal)
+      return false
+    return true
