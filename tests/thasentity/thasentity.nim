55235559d18eb21908958e6966013241cc76214e
thindil <thindil@laeran.pl>
2023-03-28 04:23:07 +0000

    tests: test for hasEntity rule should check the whole output
    FossilOrigin-Name: 131d3b0b49cca743ffa43a26f828eff58ab516e0905fac1b138130dcb4da1a7d

--- tests/thasentity/thasentity.nim
+++ tests/thasentity/thasentity.nim
@@ -1,6 +1,15 @@
 discard """
   exitcode: 0
-  outputsub: "two, three or four options"
+  output: '''FATAL The rule hasEntity accepts two, three or four options, but not enough of them are supplied: ''.
+NOTICE Doesn't have declared nkProcDef with name 'MyProc'.
+ERROR Has declared nkProcDef with name 'MyProc' at line: 1.
+NOTICE Doesn't have declared nkProcDef with name 'MyProc'.
+NOTICE Has declared nkProcDef with name 'MyProc' at line: 1.
+NOTICE Doesn't have declared nkProcDef with name 'MyProc'.
+NOTICE Declared nkProcDef with name 'MyProc' found: 0
+NOTICE Declared nkProcDef with name 'MyProc' found: 1
+NOTICE Declared nkProcDef with name 'MyProc' found: 1
+NOTICE Declared nkProcDef with name 'MyProc' found: 0'''
 """
 
 import ../../src/rules/hasentity
