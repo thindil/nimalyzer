eb7f589c021c62e0a5cf95688e4f412e41537d3b
thindil <thindil@laeran.pl>
2023-03-28 04:23:48 +0000

    tests: test for namedParams rule should check the whole output
    FossilOrigin-Name: a053edbc56b3156b59f1dfc7cca01f6fc82dbbf11a606e7d6e47fc4092b99d71

--- tests/tnamedparams/tnamedparams.nim
+++ tests/tnamedparams/tnamedparams.nim
@@ -1,6 +1,17 @@
 discard """
   exitcode: 0
-  outputsub: "randomoption"
+  output: '''FATAL The rule namedParams doesn't accept any options, but options suplied: 'randomoption'.
+ERROR call quit line: 1 doesn't have named parameter number: 1'.
+ERROR call myProc line: 1 doesn't have named parameter number: 1'.
+NOTICE Calls which have all named parameters not found.
+NOTICE call myProc line: 1 doesn't have named parameter number: 1'.
+NOTICE Calls which not have all named parameters not found.
+NOTICE call quit line: 1 doesn't have named parameter number: 1'.
+NOTICE Calls which have all named parameters found: 0
+NOTICE Calls which have all named parameters found: 1
+NOTICE call quit line: 1 doesn't have named parameter number: 1'.
+NOTICE Calls which not have all named parameters found: 1
+NOTICE Calls which not have all named parameters found: 0'''
 """
 
 import ../../src/rules/namedparams
