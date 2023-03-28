e0cff56ff4c4d4f937d38ca0d9b047015e54fd0c
thindil <thindil@laeran.pl>
2023-03-28 04:23:23 +0000

    tests: test for hasPragma rule should check the whole output
    FossilOrigin-Name: da967aec0fdb8a6ac8407481e495ef8ff8f7863ef4b42a87a31db8715b587de3

--- tests/thaspragma/thaspragma.nim
+++ tests/thaspragma/thaspragma.nim
@@ -1,6 +1,16 @@
 discard """
   exitcode: 0
-  outputsub: "require type"
+  output: '''FATAL The rule hasPragma require type of entities to check and name(s) of pragma(s) as the option, but nothing was supplied.
+ERROR procedure MyProc line: 1 doesn't have declared any pragmas.
+ERROR procedure MyProc line: 1 has declared pragma: raises: [*.
+NOTICE The selected pragma(s) not found.
+NOTICE procedure MyProc line: 1 has declared pragma: raises: [*.
+NOTICE The selected pragma(s) not found.
+NOTICE procedure MyProc line: 1 doesn't have declared any pragmas.
+NOTICE Declared procedures with selected pragmas found: 0
+NOTICE Declared procedures with selected pragmas found: 1
+NOTICE Declared procedures with selected pragmas found: 1
+NOTICE Declared procedures with selected pragmas found: 0'''
 """
 
 import ../../src/rules/haspragma
