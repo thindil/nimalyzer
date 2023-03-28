529b679a8adce355fef67b6e1e8f0c9bedbaa1de
thindil <thindil@laeran.pl>
2023-03-28 04:22:26 +0000

    tests: test for hadDoc rule should check the whole output
    FossilOrigin-Name: 3d5e269887d271beef768d5cdfdf9f719c8818600cdd4625a3f4e541f62760a2

--- tests/thasdoc/thasdoc.nim
+++ tests/thasdoc/thasdoc.nim
@@ -1,6 +1,17 @@
 discard """
   exitcode: 0
-  outputsub: "randomoption"
+  output: '''FATAL The rule hasDoc doesn't accept any options, but options suplied: 'randomoption'.
+ERROR Module doesn't have documentation.
+ERROR Module has documentation.
+NOTICE The documentation not found.
+NOTICE Module has documentation.
+NOTICE The documentation not found.
+NOTICE Module doesn't have documentation.
+NOTICE Declared public items with documentation found: 0
+NOTICE Declared public items with documentation found: 1
+NOTICE Module doesn't have documentation.
+NOTICE Declared public items with documentation found: 1
+NOTICE Declared public items with documentation found: 0'''
 """
 
 import ../../src/rules/hasdoc
