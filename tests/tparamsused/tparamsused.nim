1a912d62f25ff786960a5e294c5b1f4c9c5098e9
thindil <thindil@laeran.pl>
2023-03-29 04:20:52 +0000

    tests: test for paramsUsed rule should check the whole output
    FossilOrigin-Name: d8f227df7e8f7bd00c7babf50ec64988b2c5c53b07ad27e31433d9ce224d5f3e

--- tests/tparamsused/tparamsused.nim
+++ tests/tparamsused/tparamsused.nim
@@ -1,6 +1,15 @@
 discard """
   exitcode: 0
-  outputsub: "randomoption"
+  output: '''FATAL The option for the rule paramsUsed must be type of enties to check, but got: 'randomoption'.
+ERROR procedure MyProc line: 1 doesn't use parameter 'arg'.
+NOTICE Procedures which uses all parameters not found.
+NOTICE Procedures which not uses all parameters not found.
+NOTICE procedure MyProc line: 1 doesn't use all parameters.
+NOTICE Procedures which uses all parameters found: 0
+NOTICE Procedures which uses all parameters found: 1
+NOTICE procedure MyProc line: 1 doesn't use all parameters.
+NOTICE Procedures which not uses all parameters found: 1
+NOTICE Procedures which not uses all parameters found: 0'''
 """
 
 import ../../src/rules/paramsused
