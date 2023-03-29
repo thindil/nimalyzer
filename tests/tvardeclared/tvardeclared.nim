09a85295899da47c7c9b06653193e6430396463c
thindil <thindil@laeran.pl>
2023-03-29 04:21:40 +0000

    tests: test for varDeclared rule should check the whole output
    FossilOrigin-Name: 50801628e4d96efb4d0313550c19852511846551e04bca94c2de927ceb07333c

--- tests/tvardeclared/tvardeclared.nim
+++ tests/tvardeclared/tvardeclared.nim
@@ -1,6 +1,17 @@
 discard """
   exitcode: 0
-  outputsub: "require type"
+  output: '''FATAL The rule varDeclared require type of declaration as the option, but nothing was supplied.
+ERROR declaration of 'i' line: 1 doesn't set type for the variable.
+ERROR declaration of i line: 1 sets the type 'int' as the type of the variable.
+NOTICE Declarations withtype declaration not found.
+NOTICE declaration of i line: 1 sets the type 'int' as the type of the variable.
+NOTICE Declarations withouttype declaration not found.
+NOTICE declaration of 'i' line: 1 doesn't set type for the variable.
+NOTICE Declarations withtype declaration found: 0
+NOTICE Declarations withtype declaration found: 1
+NOTICE declaration of 'i' line: 1 doesn't set type for the variable.
+NOTICE Declarations withouttype declaration found: 1
+NOTICE Declarations withouttype declaration found: 0'''
 """
 
 import ../../src/rules/vardeclared
