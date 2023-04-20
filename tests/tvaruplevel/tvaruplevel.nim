a2123482019e38b5740718701e5188b264276804
thindil <thindil@laeran.pl>
2023-04-20 05:01:45 +0000

    tests: added unit test for varUplevel rule
    FossilOrigin-Name: 4f7cec5a8835aabb821ff4b61ea7c2f2c9806eda956884815b229b5fa9ae91ea

---
+++ tests/tvaruplevel/tvaruplevel.nim
@@ -0,0 +1,28 @@
+discard """
+  exitcode: 0
+  output: '''FATAL The rule varuplevel requires at maximum 0 options, but 1 provided: 'something'.
+ERROR declaration of 'i' line: 1 can be updated to constant.
+ERROR declaration of a line: 1 can't be updated to let.
+NOTICE Declarations which can be upgraded not found.
+NOTICE declaration of a line: 1 can't be updated to let.
+NOTICE Declarations which can't be upgraded not found.
+NOTICE declaration of 'i' line: 1 can be updated to constant.
+NOTICE Declarations which can be upgraded found: 0
+NOTICE Declarations which can be upgraded found: 1
+NOTICE declaration of 'i' line: 1 can be updated to constant.
+NOTICE Declarations which can't be upgraded found: 1
+NOTICE Declarations which can't be upgraded found: 0'''
+"""
+
+import ../../src/rules/varuplevel
+import ../helpers.nim
+
+const
+  validOptions = @[]
+  invalidOptions = @["something"]
+  invalidNimCode = "let i: int = 1"
+  validNimCode = """
+var a: seq[int]
+a.add(1)"""
+
+runRuleTest()
