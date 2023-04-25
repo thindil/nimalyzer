5df1341e186be75e9d113ef53cdbd630eb9f404e
thindil <thindil@laeran.pl>
2023-04-25 04:12:26 +0000

    tests: fixed unit test for localHides rule
    FossilOrigin-Name: 488129dafa2076325aac87bee1dccf7b0f5739f3eca0d15b30b096623ca76b15

--- tests/tlocalhides/tlocalhides.nim
+++ tests/tlocalhides/tlocalhides.nim
@@ -2,18 +2,20 @@ discard """
   exitcode: 0
   output: '''FATAL The rule localhides requires at maximum 0 options, but 1 provided: 'randomoption'.
 ERROR declaration of 'a' line: 1 is hidden by local variable in line 3.
-ERROR declaration of a line: 1 is not hidden by local variable.
-ERROR declaration of b line: 3 is not hidden by local variable.
-ERROR declaration of a line: 3 is not hidden by local variable.
-NOTICE declaration of a line: 3 is not hidden by local variable.
-NOTICE Local declarations which hide global declarations not found.
-NOTICE declaration of a line: 1 is not hidden by local variable.
-NOTICE declaration of b line: 3 is not hidden by local variable.
+ERROR declaration of 'a' line: 3 is hidden by local variable in line 5.
+ERROR declaration of 'a' line: 1 is not hidden by local variable.
+ERROR declaration of 'b' line: 3 is not hidden by local variable.
+ERROR declaration of 'a' line: 5 is not hidden by local variable.
+NOTICE declaration of 'a' line: 5 is not hidden by local variable.
+NOTICE declaration of 'a' line: 1 is not hidden by local variable.
+NOTICE declaration of 'b' line: 3 is not hidden by local variable.
 NOTICE Local declarations which hide global declarations not found.
 NOTICE declaration of 'a' line: 1 is hidden by local variable in line 3.
+NOTICE declaration of 'a' line: 3 is hidden by local variable in line 5.
 NOTICE Local declarations which hide global declarations found: 1
 NOTICE Local declarations which hide global declarations found: 2
 NOTICE declaration of 'a' line: 1 is hidden by local variable in line 3.
+NOTICE declaration of 'a' line: 3 is hidden by local variable in line 5.
 NOTICE Local declarations which hide global declarations found: 1
 NOTICE Local declarations which hide global declarations found: 0'''
 """
@@ -27,10 +29,14 @@ const
   invalidNimCode = """
 var a = 1
 for i in 1 .. 10:
-  var a = 2"""
+  var a = 2
+  for j in 2 .. 3:
+    var a = 3"""
   validNimCode = """
 var a = 1
 for i in 1 .. 10:
   var b = 2"""
 
-runRuleTest()
+# Disable check for invalid code search as it always returns error instead of
+# positive value
+runRuleTest(disabledChecks = {invalidSearch})
