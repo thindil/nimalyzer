3cc2b67f09cee2c5851c9f40432ef2dfdb070291
thindil <thindil@laeran.pl>
2023-04-28 03:58:51 +0000

    tests: added unit test for namingConv rule
    FossilOrigin-Name: 729c3783ee78798d8bdcb6e539ad4c7847d1be6cde0c4411f62d6b46103d37d4

---
+++ tests/tnamingconv/tnamingconv.nim
@@ -0,0 +1,26 @@
+discard """
+  exitcode: 0
+  output: '''FATAL The rule namingconv requires at least 2 options, but only 0 provided: ''.
+ERROR name of 'IsThe' line: 1 doesn't follow naming convention.
+ERROR name of 'isThe' line: 1 follow naming convention.
+NOTICE Declarations which follow naming convention not found.
+NOTICE name of 'isThe' line: 1 follow naming convention.
+NOTICE Declarations which not follow naming convention not found.
+NOTICE name of 'IsThe' line: 1 doesn't follow naming convention.
+NOTICE Declarations which follow naming convention found: 0
+NOTICE Declarations which follow naming convention found: 1
+NOTICE name of 'IsThe' line: 1 doesn't follow naming convention.
+NOTICE Declarations which not follow naming convention found: 1
+NOTICE Declarations which not follow naming convention found: 0'''
+"""
+
+import ../../src/rules/namingconv
+import ../helpers.nim
+
+const
+  validOptions = @["variables", "[a-z][a-zA-Z0-9_]"]
+  invalidOptions = @[]
+  invalidNimCode = "var IsThe: int = 1"
+  validNimCode = "var isThe: int = 1"
+
+runRuleTest()
