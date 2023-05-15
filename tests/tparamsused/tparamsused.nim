5135abb0574d2d6f9414b90650ac033a32ea0ca4
thindil <thindil@laeran.pl>
2023-05-15 04:03:40 +0000

    tests: fixed unit test for paramsUsed rule
    FossilOrigin-Name: 7df9296a03bfdd3a0537c75f8ea1a1696e5c79ae974c1aaa11673546e22a743c

--- tests/tparamsused/tparamsused.nim
+++ tests/tparamsused/tparamsused.nim
@@ -2,7 +2,9 @@ discard """
   exitcode: 0
   output: '''FATAL The rule paramsused option number 1 has invalid value: 'randomoption'.
 ERROR procedure MyProc line: 1 doesn't use parameter 'arg'.
+ERROR procedure MyProc line: 1 use all parameters.
 NOTICE Procedures which uses all parameters not found.
+NOTICE procedure MyProc line: 1 use all parameters.
 NOTICE Procedures which not uses all parameters not found.
 NOTICE procedure MyProc line: 1 doesn't use all parameters.
 NOTICE Procedures which uses all parameters found: 0
