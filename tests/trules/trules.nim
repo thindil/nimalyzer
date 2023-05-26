69326ebe8d7049d9c444c5e1d7c1d043912a40eb
thindil <thindil@laeran.pl>
2023-05-26 04:29:10 +0000

    tests: added unit test for getNodesToCheck procedure
    FossilOrigin-Name: 8ee069fe87aaa152a7c98c22d2563935387b12d3cb60c7f934338d5498d96ecc

--- tests/trules/trules.nim
+++ tests/trules/trules.nim
@@ -7,6 +7,7 @@ FATAL The rule Test rule requires at least 2 options, but only 1 provided: 'a'.'
 """
 
 import std/logging
+import compiler/[parser, trees]
 import ../../src/rules
 import ../helpers
 
@@ -22,10 +23,20 @@ assert resultValue == 0
 try:
   var i: int8 = int8.high
   i.inc
-except:
+except OverflowDefect:
   assert errorMessage("Reason: ", getCurrentException()) == 0
 
 assert validateOptions(RuleSettings(name: "Test Rule", options: @[integer],
     minOptions: 1), @["1"])
 assert not validateOptions(RuleSettings(name: "Test rule", options: @[str,
     integer], minOptions: 2), @["a"])
+
+let
+  (nimCache, nimConfig) = setNim()
+  parentNode = parseString("""
+  proc hello() =
+    var a = 1
+    for i in 1..2:
+      echo a""", nimCache, nimConfig)
+
+assert getNodesToCheck(parentNode, parentNode[0][6]) == flattenStmts(parentNode)
