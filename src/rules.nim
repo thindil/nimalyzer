ac8c0751186084f80c16d4b1d4ee8ea7095e628f
thindil <thindil@laeran.pl>
2023-05-30 03:53:30 +0000

    refactor: made ruleFix public for tests
    FossilOrigin-Name: 2d5259e85b47642e2ff620de6d66a6ae3796f7c59679c57fca478fa3332921ed

--- src/rules.nim
+++ src/rules.nim
@@ -422,7 +422,8 @@ macro ruleConfig*(ruleName, ruleFoundMessage, ruleNotFoundMessage,
       children = [newIdentNode(i = "rule"), nnkVarTy.newTree(children = [
       newIdentNode(i = "RuleOptions")]), newEmptyNode()])]), newEmptyNode(),
       newEmptyNode(), newEmptyNode()]), nnkStmtList.newTree(children = [
-      nnkProcDef.newTree(children = [newIdentNode(i = "ruleFix"), newEmptyNode(),
+      nnkProcDef.newTree(children = [nnkPostfix.newTree(children = [
+      newIdentNode(i = "*"), newIdentNode(i = "ruleFix")]), newEmptyNode(),
       newEmptyNode(), nnkFormalParams.newTree(children = [newIdentNode(
       i = "bool"), nnkIdentDefs.newTree(children = [newIdentNode(i = "astNode"),
       newIdentNode(i = "parentNode"), newIdentNode(i = "PNode"), newEmptyNode()]),
