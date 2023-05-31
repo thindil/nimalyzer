8fcbb172ef814040cb1b28042bb5815f2db3d815
thindil <thindil@laeran.pl>
2023-05-31 03:49:39 +0000

    tests: updated unit test for hasPragma rule
    FossilOrigin-Name: 46d7d372ad3c8fb32804579bbbc2e95e5732ba55f736a53ba8c4f281ce34f556

--- tests/thaspragma/thaspragma.nim
+++ tests/thaspragma/thaspragma.nim
@@ -2,15 +2,17 @@ discard """
   exitcode: 0
   output: '''FATAL The rule haspragma requires at least 2 options, but only 0 provided: ''.
 ERROR procedure MyProc line: 1 doesn't have declared any pragmas.
-ERROR procedure MyProc line: 1 has declared pragma: raises: [*.
+ERROR procedure MyProc line: 1 has declared pragma: raises: [].
 NOTICE The selected pragma(s) not found.
-NOTICE procedure MyProc line: 1 has declared pragma: raises: [*.
+NOTICE procedure MyProc line: 1 has declared pragma: raises: [].
 NOTICE The selected pragma(s) not found.
 NOTICE procedure MyProc line: 1 doesn't have declared any pragmas.
 NOTICE Declared procedures with selected pragmas found: 0
 NOTICE Declared procedures with selected pragmas found: 1
 NOTICE Declared procedures with selected pragmas found: 1
-NOTICE Declared procedures with selected pragmas found: 0'''
+NOTICE Declared procedures with selected pragmas found: 0
+ERROR procedure MyProc line: 1 doesn't have declared pragma: raises: [].
+ERROR procedure MyProc line: 1 has declared pragma: raises: [].'''
 """
 
 import ../../src/rules/haspragma
