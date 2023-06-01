1c2d2140883dda368656e2a74fd9ef1cc44f9586
thindil <thindil@laeran.pl>
2023-06-01 04:32:07 +0000

    test: updated test for localHides rule
    FossilOrigin-Name: f8bb30b03eab206ffcd6189f15f73d2d8fb8b451aa762e0cf92cd6d92ade7e2d

--- tests/tlocalhides/tlocalhides.nim
+++ tests/tlocalhides/tlocalhides.nim
@@ -4,21 +4,25 @@ discard """
 ERROR declaration of 'a' line: 1 is hidden by local variable in line 3.
 ERROR declaration of 'a' line: 3 is hidden by local variable in line 5.
 ERROR declaration of 'a' line: 1 is not hidden by local variable.
-ERROR declaration of 'b' line: 3 is not hidden by local variable.
+ERROR declaration of 'locala' line: 3 is not hidden by local variable.
+ERROR declaration of 'locallocala' line: 5 is not hidden by local variable.
 ERROR declaration of 'a' line: 5 is not hidden by local variable.
 NOTICE declaration of 'a' line: 5 is not hidden by local variable.
 The test for searching for invalid code is disabled.
 NOTICE declaration of 'a' line: 1 is not hidden by local variable.
-NOTICE declaration of 'b' line: 3 is not hidden by local variable.
+NOTICE declaration of 'locala' line: 3 is not hidden by local variable.
+NOTICE declaration of 'locallocala' line: 5 is not hidden by local variable.
 NOTICE Local declarations which hide global declarations not found.
 NOTICE declaration of 'a' line: 1 is hidden by local variable in line 3.
 NOTICE declaration of 'a' line: 3 is hidden by local variable in line 5.
 NOTICE Local declarations which hide global declarations found: 1
-NOTICE Local declarations which hide global declarations found: 2
+NOTICE Local declarations which hide global declarations found: 3
 NOTICE declaration of 'a' line: 1 is hidden by local variable in line 3.
 NOTICE declaration of 'a' line: 3 is hidden by local variable in line 5.
 NOTICE Local declarations which hide global declarations found: 1
 NOTICE Local declarations which hide global declarations found: 0
+ERROR declaration of 'a' line: 1 is hidden by local variable in line 3.
+ERROR declaration of 'locala' line: 3 is hidden by local variable in line 5.
 The tests for negative fix type of rule are disabled.'''
 """
 
@@ -37,8 +41,11 @@ for i in 1 .. 10:
   validNimCode = """
 var a = 1
 for i in 1 .. 10:
-  var b = 2"""
+  var locala = 2
+  for j in 2 .. 3:
+    var locallocala = 3"""
 
 # Disable check for invalid code search as it always returns error instead of
-# positive value
+# positive value. Also, disable check for negative fix type of rule as it
+# do nothing.
 runRuleTest(disabledChecks = {invalidSearch, negativeFix})
