81e6d84e90a49032c87e77bf4bff49266fd2eb39
thindil <thindil@laeran.pl>
2023-06-02 04:17:24 +0000

    tests: updated test for varUplevel rule
    FossilOrigin-Name: 21a9e2f71add8107c5a995d8be13d71a1cc516bf326eef9d8c2b56c3929f3b36

--- tests/tvaruplevel/tvaruplevel.nim
+++ tests/tvaruplevel/tvaruplevel.nim
@@ -1,17 +1,20 @@
 discard """
   exitcode: 0
   output: '''FATAL The rule varuplevel requires at maximum 0 options, but 1 provided: 'something'.
-ERROR declaration of 'i' line: 1 can be updated to constant.
+ERROR declaration of 'b' line: 3 can be updated to constant.
 ERROR declaration of a line: 1 can't be updated to let.
-NOTICE Declarations which can be upgraded not found.
+ERROR declaration of a line: 1 can't be updated to let.
+NOTICE declaration of a line: 1 can't be updated to let.
+The test for searching for invalid code is disabled.
 NOTICE declaration of a line: 1 can't be updated to let.
 NOTICE Declarations which can't be upgraded not found.
-NOTICE declaration of 'i' line: 1 can be updated to constant.
-NOTICE Declarations which can be upgraded found: 0
+NOTICE declaration of 'b' line: 3 can be updated to constant.
 NOTICE Declarations which can be upgraded found: 1
-NOTICE declaration of 'i' line: 1 can be updated to constant.
+NOTICE Declarations which can be upgraded found: 1
+NOTICE declaration of 'b' line: 3 can be updated to constant.
 NOTICE Declarations which can't be upgraded found: 1
 NOTICE Declarations which can't be upgraded found: 0
+ERROR declaration of 'b' line: 3 can be updated to constant.
 The tests for negative fix type of rule are disabled.'''
 """
 
@@ -30,4 +33,4 @@ var a: seq[int]
 a.add(1)
 const b: int = 3"""
 
-runRuleTest(disabledChecks = {negativeFix})
+runRuleTest(disabledChecks = {invalidSearch, negativeFix})
