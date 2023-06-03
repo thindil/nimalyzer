09d0a9e4a3d0382c78cadced83cca3b0c3b07f13
thindil <thindil@laeran.pl>
2023-06-03 04:37:24 +0000

    tests: updated unit test for hasDoc rule
    FossilOrigin-Name: d8ca92c0e25f091a4d17434f75fa5ca55cc49273c17007c89f80b20858095fd7

--- tests/thasdoc/thasdoc.nim
+++ tests/thasdoc/thasdoc.nim
@@ -1,18 +1,26 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule hasdoc requires at maximum 0 options, but 1 provided: 'randomoption'.
-ERROR Module doesn't have documentation.
-ERROR Module has documentation.
-NOTICE The documentation not found.
-NOTICE Module has documentation.
-NOTICE The documentation not found.
-NOTICE Module doesn't have documentation.
-NOTICE Declared public items with documentation found: 0
-NOTICE Declared public items with documentation found: 1
-NOTICE Module doesn't have documentation.
-NOTICE Declared public items with documentation found: 1
-NOTICE Declared public items with documentation found: 0
-The tests for fix type of rule are disabled.'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule hasdoc requires at maximum 0 options, but 1 provided: 'randomoption'.
+INFO: Checking check type of the rule.
+ERROR: Module doesn't have documentation.
+INFO: Checking negative check type of the rule.
+ERROR: Module has documentation.
+INFO: Checking search type of the rule.
+NOTICE: The documentation not found.
+NOTICE: Module has documentation.
+INFO: Checking negative search type of the rule.
+NOTICE: The documentation not found.
+NOTICE: Module doesn't have documentation.
+INFO: Checking count type of the rule.
+NOTICE: Declared public items with documentation found: 0
+NOTICE: Declared public items with documentation found: 1
+INFO: Checking negative type of the rule.
+NOTICE: Module doesn't have documentation.
+NOTICE: Declared public items with documentation found: 1
+NOTICE: Declared public items with documentation found: 0
+INFO: Checking fix type of the rule.
+INFO: The tests for fix type of rule are disabled.'''
 """
 
 import ../../src/rules/hasdoc
