a517b98a2255237207d87471b52883afdc6182dc
thindil <thindil@laeran.pl>
2023-06-03 04:38:12 +0000

    tests: updated unit test for namedParams rule
    FossilOrigin-Name: aea4f2578efe1bcd197dbeb1f980d413cec5a7c560ece16a91af8ebdf7b2c2a7

--- tests/tnamedparams/tnamedparams.nim
+++ tests/tnamedparams/tnamedparams.nim
@@ -1,18 +1,26 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule namedparams requires at maximum 0 options, but 1 provided: 'randomoption'.
-ERROR call quit line: 1 doesn't have named parameter number: 1'.
-ERROR call myProc line: 1 doesn't have named parameter number: 1'.
-NOTICE Calls which have all named parameters not found.
-NOTICE call myProc line: 1 doesn't have named parameter number: 1'.
-NOTICE Calls which not have all named parameters not found.
-NOTICE call quit line: 1 doesn't have named parameter number: 1'.
-NOTICE Calls which have all named parameters found: 0
-NOTICE Calls which have all named parameters found: 1
-NOTICE call quit line: 1 doesn't have named parameter number: 1'.
-NOTICE Calls which not have all named parameters found: 1
-NOTICE Calls which not have all named parameters found: 0
-The tests for fix type of rule are disabled.'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule namedparams requires at maximum 0 options, but 1 provided: 'randomoption'.
+INFO: Checking check type of the rule.
+ERROR: call quit line: 1 doesn't have named parameter number: 1'.
+INFO: Checking negative check type of the rule.
+ERROR: call myProc line: 1 doesn't have named parameter number: 1'.
+INFO: Checking search type of the rule.
+NOTICE: Calls which have all named parameters not found.
+NOTICE: call myProc line: 1 doesn't have named parameter number: 1'.
+INFO: Checking negative search type of the rule.
+NOTICE: Calls which not have all named parameters not found.
+NOTICE: call quit line: 1 doesn't have named parameter number: 1'.
+INFO: Checking count type of the rule.
+NOTICE: Calls which have all named parameters found: 0
+NOTICE: Calls which have all named parameters found: 1
+INFO: Checking negative type of the rule.
+NOTICE: call quit line: 1 doesn't have named parameter number: 1'.
+NOTICE: Calls which not have all named parameters found: 1
+NOTICE: Calls which not have all named parameters found: 0
+INFO: Checking fix type of the rule.
+INFO: The tests for fix type of rule are disabled.'''
 """
 
 import ../../src/rules/namedparams
