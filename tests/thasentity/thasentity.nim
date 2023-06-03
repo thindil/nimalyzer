c7cb9f5e40d1910fcbc2a347760f935567eb1dc3
thindil <thindil@laeran.pl>
2023-06-03 04:37:33 +0000

    tests: updated unit test for hasEntity rule
    FossilOrigin-Name: 771281c6d3227aa8dc2cca25d42e1467aee8b7447d583ddfe818bbcff891213b

--- tests/thasentity/thasentity.nim
+++ tests/thasentity/thasentity.nim
@@ -1,16 +1,24 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule hasentity requires at least 2 options, but only 1 provided: ''.
-ERROR Doesn't have declared nkProcDef with name 'MyProc'.
-ERROR Has declared nkProcDef with name 'MyProc' at line: 1.
-ERROR Doesn't have declared nkProcDef with name 'MyProc'.
-NOTICE Has declared nkProcDef with name 'MyProc' at line: 1.
-NOTICE Doesn't have declared nkProcDef with name 'MyProc'.
-NOTICE Declared nkProcDef with name 'MyProc' found: 0
-NOTICE Declared nkProcDef with name 'MyProc' found: 1
-NOTICE Declared nkProcDef with name 'MyProc' found: 1
-NOTICE Declared nkProcDef with name 'MyProc' found: 0
-The tests for fix type of rule are disabled.'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule hasentity requires at least 2 options, but only 1 provided: ''.
+INFO: Checking check type of the rule.
+ERROR: Doesn't have declared nkProcDef with name 'MyProc'.
+INFO: Checking negative check type of the rule.
+ERROR: Has declared nkProcDef with name 'MyProc' at line: 1.
+INFO: Checking search type of the rule.
+ERROR: Doesn't have declared nkProcDef with name 'MyProc'.
+NOTICE: Has declared nkProcDef with name 'MyProc' at line: 1.
+INFO: Checking negative search type of the rule.
+NOTICE: Doesn't have declared nkProcDef with name 'MyProc'.
+INFO: Checking count type of the rule.
+NOTICE: Declared nkProcDef with name 'MyProc' found: 0
+NOTICE: Declared nkProcDef with name 'MyProc' found: 1
+INFO: Checking negative type of the rule.
+NOTICE: Declared nkProcDef with name 'MyProc' found: 1
+NOTICE: Declared nkProcDef with name 'MyProc' found: 0
+INFO: Checking fix type of the rule.
+INFO: The tests for fix type of rule are disabled.'''
 """
 
 import ../../src/rules/hasentity
