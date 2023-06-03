6d5b8d92096ad43a62c91b562c7acd9eeb546b81
thindil <thindil@laeran.pl>
2023-06-03 04:37:57 +0000

    tests: updated unit test for localHides rule
    FossilOrigin-Name: 99409f2375a0e7b9ecb0fa58706105d7e0adf18836b80cf5431395f70dbc0844

--- tests/tlocalhides/tlocalhides.nim
+++ tests/tlocalhides/tlocalhides.nim
@@ -1,29 +1,38 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule localhides requires at maximum 0 options, but 1 provided: 'randomoption'.
-ERROR declaration of 'a' line: 1 is hidden by local variable in line 3.
-ERROR declaration of 'a' line: 3 is hidden by local variable in line 5.
-ERROR declaration of 'a' line: 1 is not hidden by local variable.
-ERROR declaration of 'locala' line: 3 is not hidden by local variable.
-ERROR declaration of 'locallocala' line: 5 is not hidden by local variable.
-ERROR declaration of 'a' line: 5 is not hidden by local variable.
-NOTICE declaration of 'a' line: 5 is not hidden by local variable.
-The test for searching for invalid code is disabled.
-NOTICE declaration of 'a' line: 1 is not hidden by local variable.
-NOTICE declaration of 'locala' line: 3 is not hidden by local variable.
-NOTICE declaration of 'locallocala' line: 5 is not hidden by local variable.
-NOTICE Local declarations which hide global declarations not found.
-NOTICE declaration of 'a' line: 1 is hidden by local variable in line 3.
-NOTICE declaration of 'a' line: 3 is hidden by local variable in line 5.
-NOTICE Local declarations which hide global declarations found: 1
-NOTICE Local declarations which hide global declarations found: 3
-NOTICE declaration of 'a' line: 1 is hidden by local variable in line 3.
-NOTICE declaration of 'a' line: 3 is hidden by local variable in line 5.
-NOTICE Local declarations which hide global declarations found: 1
-NOTICE Local declarations which hide global declarations found: 0
-ERROR declaration of 'a' line: 1 is hidden by local variable in line 3.
-ERROR declaration of 'locala' line: 3 is hidden by local variable in line 5.
-The tests for negative fix type of rule are disabled.'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule localhides requires at maximum 0 options, but 1 provided: 'randomoption'.
+INFO: Checking check type of the rule.
+ERROR: declaration of 'a' line: 1 is hidden by local variable in line 3.
+ERROR: declaration of 'a' line: 3 is hidden by local variable in line 5.
+INFO: Checking negative check type of the rule.
+ERROR: declaration of 'a' line: 1 is not hidden by local variable.
+ERROR: declaration of 'locala' line: 3 is not hidden by local variable.
+ERROR: declaration of 'locallocala' line: 5 is not hidden by local variable.
+ERROR: declaration of 'a' line: 5 is not hidden by local variable.
+INFO: Checking search type of the rule.
+NOTICE: declaration of 'a' line: 5 is not hidden by local variable.
+INFO: The test for searching for invalid code is disabled.
+NOTICE: declaration of 'a' line: 1 is not hidden by local variable.
+NOTICE: declaration of 'locala' line: 3 is not hidden by local variable.
+NOTICE: declaration of 'locallocala' line: 5 is not hidden by local variable.
+INFO: Checking negative search type of the rule.
+NOTICE: Local declarations which hide global declarations not found.
+NOTICE: declaration of 'a' line: 1 is hidden by local variable in line 3.
+NOTICE: declaration of 'a' line: 3 is hidden by local variable in line 5.
+INFO: Checking count type of the rule.
+NOTICE: Local declarations which hide global declarations found: 1
+NOTICE: Local declarations which hide global declarations found: 3
+INFO: Checking negative type of the rule.
+NOTICE: declaration of 'a' line: 1 is hidden by local variable in line 3.
+NOTICE: declaration of 'a' line: 3 is hidden by local variable in line 5.
+NOTICE: Local declarations which hide global declarations found: 1
+NOTICE: Local declarations which hide global declarations found: 0
+INFO: Checking fix type of the rule.
+ERROR: declaration of 'a' line: 1 is hidden by local variable in line 3.
+ERROR: declaration of 'locala' line: 3 is hidden by local variable in line 5.
+INFO: Checking negative fix type of the rule.
+INFO: The tests for negative fix type of rule are disabled.'''
 """
 
 import ../../src/rules/localhides
