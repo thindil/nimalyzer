d4d3f71e3dd9ed04f8118dd08d514f8be9269d8c
thindil <thindil@laeran.pl>
2023-06-03 04:37:44 +0000

    tests: updated unit test for hasPragma rule
    FossilOrigin-Name: be748a97a6fe551285145ee3f5461263cf855a3c38128205834329b670cf414c

--- tests/thaspragma/thaspragma.nim
+++ tests/thaspragma/thaspragma.nim
@@ -1,18 +1,27 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule haspragma requires at least 2 options, but only 0 provided: ''.
-ERROR procedure MyProc line: 1 doesn't have declared any pragmas.
-ERROR procedure MyProc line: 1 has declared pragma: raises: [].
-NOTICE The selected pragma(s) not found.
-NOTICE procedure MyProc line: 1 has declared pragma: raises: [].
-NOTICE The selected pragma(s) not found.
-NOTICE procedure MyProc line: 1 doesn't have declared any pragmas.
-NOTICE Declared procedures with selected pragmas found: 0
-NOTICE Declared procedures with selected pragmas found: 1
-NOTICE Declared procedures with selected pragmas found: 1
-NOTICE Declared procedures with selected pragmas found: 0
-ERROR procedure MyProc line: 1 doesn't have declared pragma: raises: [].
-ERROR procedure MyProc line: 1 has declared pragma: raises: [].'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule haspragma requires at least 2 options, but only 0 provided: ''.
+INFO: Checking check type of the rule.
+ERROR: procedure MyProc line: 1 doesn't have declared any pragmas.
+INFO: Checking negative check type of the rule.
+ERROR: procedure MyProc line: 1 has declared pragma: raises: [].
+INFO: Checking search type of the rule.
+NOTICE: The selected pragma(s) not found.
+NOTICE: procedure MyProc line: 1 has declared pragma: raises: [].
+INFO: Checking negative search type of the rule.
+NOTICE: The selected pragma(s) not found.
+NOTICE: procedure MyProc line: 1 doesn't have declared any pragmas.
+INFO: Checking count type of the rule.
+NOTICE: Declared procedures with selected pragmas found: 0
+NOTICE: Declared procedures with selected pragmas found: 1
+INFO: Checking negative type of the rule.
+NOTICE: Declared procedures with selected pragmas found: 1
+NOTICE: Declared procedures with selected pragmas found: 0
+INFO: Checking fix type of the rule.
+ERROR: procedure MyProc line: 1 doesn't have declared pragma: raises: [].
+INFO: Checking negative fix type of the rule.
+ERROR: procedure MyProc line: 1 has declared pragma: raises: [].'''
 """
 
 import ../../src/rules/haspragma
