0f656fa8df84f1fc4ae14604b23d15e0a4b985dd
thindil <thindil@laeran.pl>
2023-06-04 04:03:32 +0000

    tests: updated unit test for paramsUsed rule
    FossilOrigin-Name: d478bc00f810566045a7bc9e5d0de6d20aa4f74fb47d2dd89b667ac210bc6407

--- tests/tparamsused/tparamsused.nim
+++ tests/tparamsused/tparamsused.nim
@@ -1,18 +1,26 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule paramsused option number 1 has invalid value: 'randomoption'.
-ERROR procedure MyProc line: 1 doesn't use parameter 'arg'.
-ERROR procedure MyProc line: 1 use all parameters.
-NOTICE Procedures which uses all parameters not found.
-NOTICE procedure MyProc line: 1 use all parameters.
-NOTICE Procedures which not uses all parameters not found.
-NOTICE procedure MyProc line: 1 doesn't use all parameters.
-NOTICE Procedures which uses all parameters found: 0
-NOTICE Procedures which uses all parameters found: 1
-NOTICE procedure MyProc line: 1 doesn't use all parameters.
-NOTICE Procedures which not uses all parameters found: 1
-NOTICE Procedures which not uses all parameters found: 0
-The tests for fix type of rule are disabled.'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule paramsused option number 1 has invalid value: 'randomoption'.
+INFO: Checking check type of the rule.
+ERROR: procedure MyProc line: 1 doesn't use parameter 'arg'.
+INFO: Checking negative check type of the rule.
+ERROR: procedure MyProc line: 1 use all parameters.
+INFO: Checking search type of the rule.
+NOTICE: Procedures which uses all parameters not found.
+NOTICE: procedure MyProc line: 1 use all parameters.
+INFO: Checking negative search type of the rule.
+NOTICE: Procedures which not uses all parameters not found.
+NOTICE: procedure MyProc line: 1 doesn't use all parameters.
+INFO: Checking count type of the rule.
+NOTICE: Procedures which uses all parameters found: 0
+NOTICE: Procedures which uses all parameters found: 1
+INFO: Checking negative type of the rule.
+NOTICE: procedure MyProc line: 1 doesn't use all parameters.
+NOTICE: Procedures which not uses all parameters found: 1
+NOTICE: Procedures which not uses all parameters found: 0
+INFO: Checking fix type of the rule.
+INFO: The tests for fix type of rule are disabled.'''
 """
 
 import ../../src/rules/paramsused
