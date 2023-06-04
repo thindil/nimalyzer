b6d4864a218f19bd24465331ee5c9f51bd4d101b
thindil <thindil@laeran.pl>
2023-06-04 04:03:47 +0000

    tests: updated unit test for varDeclared rule
    FossilOrigin-Name: 46109262ac8fb043a312ba075a07303a5491939da3abb6736067b79ad8603a0a

--- tests/tvardeclared/tvardeclared.nim
+++ tests/tvardeclared/tvardeclared.nim
@@ -1,18 +1,26 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule vardeclared requires at least 1 options, but only 0 provided: ''.
-ERROR declaration of 'i' line: 1 doesn't set type for the variable.
-ERROR declaration of i line: 1 sets the type 'int' as the type of the variable.
-NOTICE Declarations withtype declaration not found.
-NOTICE declaration of i line: 1 sets the type 'int' as the type of the variable.
-NOTICE Declarations withouttype declaration not found.
-NOTICE declaration of 'i' line: 1 doesn't set type for the variable.
-NOTICE Declarations withtype declaration found: 0
-NOTICE Declarations withtype declaration found: 1
-NOTICE declaration of 'i' line: 1 doesn't set type for the variable.
-NOTICE Declarations withouttype declaration found: 1
-NOTICE Declarations withouttype declaration found: 0
-The tests for fix type of rule are disabled.'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule vardeclared requires at least 1 options, but only 0 provided: ''.
+INFO: Checking check type of the rule.
+ERROR: declaration of 'i' line: 1 doesn't set type for the variable.
+INFO: Checking negative check type of the rule.
+ERROR: declaration of i line: 1 sets the type 'int' as the type of the variable.
+INFO: Checking search type of the rule.
+NOTICE: Declarations withtype declaration not found.
+NOTICE: declaration of i line: 1 sets the type 'int' as the type of the variable.
+INFO: Checking negative search type of the rule.
+NOTICE: Declarations withouttype declaration not found.
+NOTICE: declaration of 'i' line: 1 doesn't set type for the variable.
+INFO: Checking count type of the rule.
+NOTICE: Declarations withtype declaration found: 0
+NOTICE: Declarations withtype declaration found: 1
+INFO: Checking negative type of the rule.
+NOTICE: declaration of 'i' line: 1 doesn't set type for the variable.
+NOTICE: Declarations withouttype declaration found: 1
+NOTICE: Declarations withouttype declaration found: 0
+INFO: Checking fix type of the rule.
+INFO: The tests for fix type of rule are disabled.'''
 """
 
 import ../../src/rules/vardeclared
