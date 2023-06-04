a699856a1b0eff0f452855c869f1691255dce6a3
thindil <thindil@laeran.pl>
2023-06-04 04:03:17 +0000

    tests: updated unit test for namingConv rule
    FossilOrigin-Name: 16fff31cb05c540a89702496a527af4e606743236e66c2de5fe61aedb2317af8

--- tests/tnamingconv/tnamingconv.nim
+++ tests/tnamingconv/tnamingconv.nim
@@ -1,18 +1,26 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule namingconv requires at least 2 options, but only 0 provided: ''.
-ERROR name of 'IsThe' line: 1 doesn't follow naming convention.
-ERROR name of 'isThe' line: 1 follow naming convention.
-NOTICE Declarations which follow naming convention not found.
-NOTICE name of 'isThe' line: 1 follow naming convention.
-NOTICE Declarations which not follow naming convention not found.
-NOTICE name of 'IsThe' line: 1 doesn't follow naming convention.
-NOTICE Declarations which follow naming convention found: 0
-NOTICE Declarations which follow naming convention found: 1
-NOTICE name of 'IsThe' line: 1 doesn't follow naming convention.
-NOTICE Declarations which not follow naming convention found: 1
-NOTICE Declarations which not follow naming convention found: 0
-The tests for fix type of rule are disabled.'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule namingconv requires at least 2 options, but only 0 provided: ''.
+INFO: Checking check type of the rule.
+ERROR: name of 'IsThe' line: 1 doesn't follow naming convention.
+INFO: Checking negative check type of the rule.
+ERROR: name of 'isThe' line: 1 follow naming convention.
+INFO: Checking search type of the rule.
+NOTICE: Declarations which follow naming convention not found.
+NOTICE: name of 'isThe' line: 1 follow naming convention.
+INFO: Checking negative search type of the rule.
+NOTICE: Declarations which not follow naming convention not found.
+NOTICE: name of 'IsThe' line: 1 doesn't follow naming convention.
+INFO: Checking count type of the rule.
+NOTICE: Declarations which follow naming convention found: 0
+NOTICE: Declarations which follow naming convention found: 1
+INFO: Checking negative type of the rule.
+NOTICE: name of 'IsThe' line: 1 doesn't follow naming convention.
+NOTICE: Declarations which not follow naming convention found: 1
+NOTICE: Declarations which not follow naming convention found: 0
+INFO: Checking fix type of the rule.
+INFO: The tests for fix type of rule are disabled.'''
 """
 
 import ../../src/rules/namingconv
