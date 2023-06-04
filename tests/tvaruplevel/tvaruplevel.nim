7418f8f7e4e9e00967e6ffada9f831cb010d7479
thindil <thindil@laeran.pl>
2023-06-04 04:04:03 +0000

    tests: updated unit test for varUplevel rule
    FossilOrigin-Name: 8cd026b6fd2a6730c4bd0b2ef01c228a74a4d083eb63280985c7dfc3c682ed69

--- tests/tvaruplevel/tvaruplevel.nim
+++ tests/tvaruplevel/tvaruplevel.nim
@@ -1,21 +1,30 @@
 discard """
   exitcode: 0
-  output: '''FATAL The rule varuplevel requires at maximum 0 options, but 1 provided: 'something'.
-ERROR declaration of 'b' line: 3 can be updated to constant.
-ERROR declaration of a line: 1 can't be updated to let.
-ERROR declaration of a line: 1 can't be updated to let.
-NOTICE declaration of a line: 1 can't be updated to let.
-The test for searching for invalid code is disabled.
-NOTICE declaration of a line: 1 can't be updated to let.
-NOTICE Declarations which can't be upgraded not found.
-NOTICE declaration of 'b' line: 3 can be updated to constant.
-NOTICE Declarations which can be upgraded found: 1
-NOTICE Declarations which can be upgraded found: 1
-NOTICE declaration of 'b' line: 3 can be updated to constant.
-NOTICE Declarations which can't be upgraded found: 1
-NOTICE Declarations which can't be upgraded found: 0
-ERROR declaration of 'b' line: 3 can be updated to constant.
-The tests for negative fix type of rule are disabled.'''
+  output: '''INFO: Checking the rule's options validation.
+FATAL: The rule varuplevel requires at maximum 0 options, but 1 provided: 'something'.
+INFO: Checking check type of the rule.
+ERROR: declaration of 'b' line: 3 can be updated to constant.
+INFO: Checking negative check type of the rule.
+ERROR: declaration of a line: 1 can't be updated to let.
+ERROR: declaration of a line: 1 can't be updated to let.
+INFO: Checking search type of the rule.
+NOTICE: declaration of a line: 1 can't be updated to let.
+INFO: The test for searching for invalid code is disabled.
+NOTICE: declaration of a line: 1 can't be updated to let.
+INFO: Checking negative search type of the rule.
+NOTICE: Declarations which can't be upgraded not found.
+NOTICE: declaration of 'b' line: 3 can be updated to constant.
+INFO: Checking count type of the rule.
+NOTICE: Declarations which can be upgraded found: 1
+NOTICE: Declarations which can be upgraded found: 1
+INFO: Checking negative type of the rule.
+NOTICE: declaration of 'b' line: 3 can be updated to constant.
+NOTICE: Declarations which can't be upgraded found: 1
+NOTICE: Declarations which can't be upgraded found: 0
+INFO: Checking fix type of the rule.
+ERROR: declaration of 'b' line: 3 can be updated to constant.
+INFO: Checking negative fix type of the rule.
+INFO: The tests for negative fix type of rule are disabled.'''
 """
 
 import ../../src/rules/varuplevel
