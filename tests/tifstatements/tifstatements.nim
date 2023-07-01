bb9351242b3c82bff7f724e19ad05a43e68d6e1e
thindil <thindil@laeran.pl>
2023-07-01 04:37:05 +0000

    tests: updated unit test for ifStatements rule
    FossilOrigin-Name: 6e2f90ff5ab1251eba0959cd34a038078791c9993121b29f4a6a31ebc90fd7d5

--- tests/tifstatements/tifstatements.nim
+++ tests/tifstatements/tifstatements.nim
@@ -2,22 +2,26 @@ discard """
   exitcode: 0
   output: '''INFO: Checking the rule's options validation.
 FATAL: The rule ifstatements requires at maximum 0 options, but 1 provided: 'randomoption'.
-INFO: Checking check type of the rule.
+INFO: Checking check type of the rule with the invalid code.
 ERROR: if statement, line: 1 the if statement starts with a negative condition.
-INFO: Checking negative check type of the rule.
+INFO: Checking check type of the rule with the valid code.
+INFO: Checking negative check type of the rule with the valid code.
 ERROR: if statement, line: 1 the if statement branch doesn't contain only discard statement.
-ERROR: if statement, line: 1 the if statement branch doesn't contain only discard statement.
-INFO: Checking search type of the rule.
+INFO: Checking negative check type of the rule with the invalid code.
+INFO: Checking search type of the rule with the invalid code.
 NOTICE: if statement, line: 1 the if statement starts with a negative condition.
-INFO: Checking negative search type of the rule.
+INFO: Checking search type of the rule with the valid code.
+INFO: Checking negative search type of the rule with the valid code.
 NOTICE: if statement, line: 1 the if statement branch doesn't contain only discard statement.
-NOTICE: if statement, line: 1 the if statement branch doesn't contain only discard statement.
-INFO: Checking count type of the rule.
+INFO: Checking negative search type of the rule with the invalid code.
+NOTICE: If statements which can't be upgraded not found.
+INFO: Checking count type of the rule with the invalid code.
 NOTICE: If statements which can be upgraded found: 1
+INFO: Checking count type of the rule with the valid code.
 NOTICE: If statements which can be upgraded found: 0
-INFO: Checking negative type of the rule.
-NOTICE: if statement, line: 1 the if statement branch doesn't contain only discard statement.
+INFO: Checking negative count type of the rule with the invalid code.
 NOTICE: If statements which can't be upgraded found: 0
+INFO: Checking negative count type of the rule with the valid code.
 NOTICE: if statement, line: 1 the if statement branch doesn't contain only discard statement.
 NOTICE: If statements which can't be upgraded found: 1
 INFO: Checking fix type of the rule.
