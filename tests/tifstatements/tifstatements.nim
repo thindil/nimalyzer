406864794168d2b152464328b7be7545c0b65be9
thindil <thindil@laeran.pl>
2023-07-02 04:06:31 +0000

    tests: updated unit test for ifStatements rule
    FossilOrigin-Name: 5e501de24e199f12ebebc3ba7e3516c7820b60be76502abb0f445fc9dff874d7

--- tests/tifstatements/tifstatements.nim
+++ tests/tifstatements/tifstatements.nim
@@ -9,21 +9,23 @@ INFO: Checking negative check type of the rule with the valid code.
 ERROR: if statement, line: 1 the if statement branch doesn't contain only discard statement.
 INFO: Checking negative check type of the rule with the invalid code.
 INFO: Checking search type of the rule with the invalid code.
-NOTICE: if statement, line: 1 the if statement starts with a negative condition.
+NOTICE: if statement, line: 1 the if statement branch contains only discard statement.
+INFO: The test for searching for invalid code is disabled.
 INFO: Checking search type of the rule with the valid code.
+NOTICE: if statement, line: 1 the if statement branch contains only discard statement.
 INFO: Checking negative search type of the rule with the valid code.
-NOTICE: if statement, line: 1 the if statement branch doesn't contain only discard statement.
-INFO: Checking negative search type of the rule with the invalid code.
 NOTICE: If statements which can't be upgraded not found.
+INFO: Checking negative search type of the rule with the invalid code.
+NOTICE: if statement, line: 1 the if statement doesn't start with a negative condition.
 INFO: Checking count type of the rule with the invalid code.
 NOTICE: If statements which can be upgraded found: 1
 INFO: Checking count type of the rule with the valid code.
 NOTICE: If statements which can be upgraded found: 0
 INFO: Checking negative count type of the rule with the invalid code.
-NOTICE: If statements which can't be upgraded found: 0
-INFO: Checking negative count type of the rule with the valid code.
-NOTICE: if statement, line: 1 the if statement branch doesn't contain only discard statement.
+NOTICE: if statement, line: 1 the if statement doesn't start with a negative condition.
 NOTICE: If statements which can't be upgraded found: 1
+INFO: Checking negative count type of the rule with the valid code.
+NOTICE: If statements which can't be upgraded found: 0
 INFO: Checking fix type of the rule.
 ERROR: if statement, line: 1 the if statement starts with a negative condition.
 INFO: Checking negative fix type of the rule.
@@ -39,4 +41,4 @@ const
   invalidNimCode = "if a != 1: echo \"not equal\" else: echo \"equal\""
   validNimCode = "if a == 1: echo \"equal\" else: echo \"not equal\""
 
-runRuleTest(disabledChecks = {negativeFix})
+runRuleTest(disabledChecks = {negativeFix, invalidSearch})
