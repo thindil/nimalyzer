22b7d6d7febafa1f8b30d5f888755979e56fbaa3
thindil <thindil@laeran.pl>
2023-07-09 04:23:43 +0000

    tests: updated unit test for ifStatements rule
    FossilOrigin-Name: 31a8acce42de1cb63dd8b5fb4f6bac4d69f897819608bd94ade725c04de4ff46

--- tests/tifstatements/tifstatements.nim
+++ tests/tifstatements/tifstatements.nim
@@ -3,31 +3,31 @@ discard """
   output: '''INFO: Checking the rule's options validation.
 FATAL: The rule ifstatements requires at maximum 0 options, but 1 provided: 'randomoption'.
 INFO: Checking check type of the rule with the invalid code.
-ERROR: if statement, line: 1 the if statement starts with a negative condition.
+ERROR: if statement, line: 1 starts with a negative condition.
 INFO: Checking check type of the rule with the valid code.
 INFO: Checking negative check type of the rule with the valid code.
-ERROR: if statement, line: 1 the if statement branch doesn't contain only discard statement.
+ERROR: if statement, line: 1 the statement branch doesn't contain only discard statement.
 INFO: Checking negative check type of the rule with the invalid code.
 INFO: Checking search type of the rule with the invalid code.
-NOTICE: if statement, line: 1 the if statement branch contains only discard statement.
+NOTICE: if statement, line: 1 the statement branch contains only discard statement.
 INFO: The test for searching for invalid code is disabled.
 INFO: Checking search type of the rule with the valid code.
-NOTICE: if statement, line: 1 the if statement branch contains only discard statement.
+NOTICE: if statement, line: 1 the statement branch contains only discard statement.
 INFO: Checking negative search type of the rule with the valid code.
 NOTICE: If statements which can't be upgraded not found.
 INFO: Checking negative search type of the rule with the invalid code.
-NOTICE: if statement, line: 1 the if statement doesn't start with a negative condition.
+NOTICE: if statement, line: 1 doesn't start with a negative condition.
 INFO: Checking count type of the rule with the invalid code.
 NOTICE: If statements which can be upgraded found: 1
 INFO: Checking count type of the rule with the valid code.
 NOTICE: If statements which can be upgraded found: 0
 INFO: Checking negative count type of the rule with the invalid code.
-NOTICE: if statement, line: 1 the if statement doesn't start with a negative condition.
+NOTICE: if statement, line: 1 doesn't start with a negative condition.
 NOTICE: If statements which can't be upgraded found: 1
 INFO: Checking negative count type of the rule with the valid code.
 NOTICE: If statements which can't be upgraded found: 0
 INFO: Checking fix type of the rule.
-ERROR: if statement, line: 1 the if statement starts with a negative condition.
+ERROR: if statement, line: 1 starts with a negative condition.
 INFO: Checking negative fix type of the rule.
 INFO: The tests for negative fix type of rule are disabled.'''
 """
