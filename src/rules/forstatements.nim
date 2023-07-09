c20ce4f2548595676a5a917162c31fda5ecaa4e1
thindil <thindil@laeran.pl>
2023-07-09 04:24:28 +0000

    docs: added documentation for forStatements rule
    FossilOrigin-Name: 4b6be93cf04f4c51538b0152e7fdb47617b49c030f3fd1fcd20c94a0078c7a5a

--- src/rules/forstatements.nim
+++ src/rules/forstatements.nim
@@ -23,42 +23,58 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-## --Insert here the description of the rule--
+## The rule to check do `for` statements in the code don't contain some
+## expressions. At the moment, it can check only if the `for` statement iterator
+## has or doesn't have the direct call to `pairs` or `items` iterators.
+##
 ## The syntax in a configuration file is::
 ##
 ##   [ruleType] ?not? forStatements
 ##
 ## * ruleType is the type of rule which will be executed. Proper values are:
 ##   *check*, *search*, *count* and *fix*. For more information about the types of
-##   rules, please refer to the program's documentation. --Insert description
-##   how rules types works with the rule--.
+##   rules, please refer to the program's documentation. Check type will raise
+##   an error if there is a `for` statement which violates the check. Search
+##   type will list all statements which violates the check or raise an
+##   error if nothing found. Count type will simply list the amount of the
+##   statements which violates the check. Fix type will try to fix the code
+##   which violates check: it will add the direct call for proper iterator or
+##   remove it, for negation type of the rule.
 ## * optional word *not* means negation for the rule. Adding word *not* will
-##   change to inform only about --Insert description how negation affects the
-##   rule--.
+##   change to inform only about the `for` statements which not violates the
+##   rule's check.
 ## * forStatements is the name of the rule. It is case-insensitive, thus it can be
-##   set as *forstatements*, *forStatements* or *--rUlEnAmE--*.
+##   set as *forstatements*, *forStatements* or *fOrStAtEmEnTs*.
 ##
 ## Disabling the rule
 ## ------------------
 ## It is possible to disable the rule for a selected part of the checked code
-## by using pragma *ruleOff: "forStatements"* in the element from which the rule
-## should be disabled or in code before it. For example, if the rule should
-## be disabled for procedure `proc main()`, the full declaration of it should
-## be::
+## by using pragma *ruleOff: "forStatements"* in the code before it. For
+## example, if the rule should be disabled for the selected statement, the full
+## declaration of it should be::
 ##
-##     proc main () {.ruleOff: "forStatements".}
+##     {.ruleOff: "forStatements".}
+##     for i in [1..5]:
+##       echo i
 ##
-## To enable the rule again, the pragma *ruleOn: "forStatements"* should be added in
-## the element which should be checked or in code before it. For example, if
-## the rule should be re-enabled for `const a = 1`, the full declaration should
-## be::
+## To enable the rule again, the pragma *ruleOn: "forStatements"* should be
+## added in the code before it. For example, if the rule should be re-enabled
+## for the statement, the full declaration should be::
 ##
-##     const a {.ruleOn: "forStatements".} = 1
+##     {.ruleOn: "forStatements".}
+##     for i in [1..5]:
+##       echo i
 ##
 ## Examples
 ## --------
 ##
-## --Insert rules examples--
+## 1. Check if all `for` statements have direct calls for iterators::
+##
+##     check forStatements
+##
+## 2. Remove all direct calls for iterators from `for` statements::
+##
+##     fix not forStatements
 
 # External modules imports
 import compiler/idents
