dbe68a96b7c135def254f4a00a176575e5b8904e
thindil <thindil@laeran.pl>
2023-07-25 04:51:37 +0000

    tests: fixed unit tests for config module
    FossilOrigin-Name: dfd1e45fb8dd8d083a787a387c768ff7bd88966053e0790223045c4d8c3bd02b

--- tests/tconfig/tconfig.nim
+++ tests/tconfig/tconfig.nim
@@ -8,5 +8,7 @@ import ../helpers
 setLogger()
 
 var sections = 0
-let (sources, rules, fixCommand) = parseConfig("config/nimalyzer.cfg", sections)
-assert sources.len > 0 and rules.len > 0 and fixCommand.len > 0 and sections == 1
+let (sources, rules, fixCommand, maxReports) = parseConfig(
+    "config/nimalyzer.cfg", sections)
+assert sources.len > 0 and rules.len > 0 and fixCommand.len > 0 and sections ==
+    1 and maxReports == Natural.high
