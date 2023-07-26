155742195b7f8dd715142dc446c20457aa42f47f
thindil <thindil@laeran.pl>
2023-07-26 04:37:20 +0000

    tests: added assert messages to unit tests for config module
    FossilOrigin-Name: 967b70179be745d59c507f8a53fb8e3f505d0040c95cb3e082dcec671194835a

--- tests/tconfig/tconfig.nim
+++ tests/tconfig/tconfig.nim
@@ -11,4 +11,4 @@ var sections = 0
 let (sources, rules, fixCommand, maxReports) = parseConfig(
     "config/nimalyzer.cfg", sections)
 assert sources.len > 0 and rules.len > 0 and fixCommand.len > 0 and sections ==
-    1 and maxReports == Natural.high
+    1 and maxReports == Natural.high, "Failed to parse the config file."
