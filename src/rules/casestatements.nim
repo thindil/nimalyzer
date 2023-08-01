3cfcdeea6f7790f315ad8efbe558e5c5d5d14074
thindil <thindil@laeran.pl>
2023-08-01 04:15:29 +0000

    docs: added documentation for caseStatements rule
    FossilOrigin-Name: 96739bd04a40227c7a583f4755e5aa7875b0382d3493c3f7a268ce1e51850833

--- src/rules/casestatements.nim
+++ src/rules/casestatements.nim
@@ -23,42 +23,64 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-## --Insert here the description of the rule--
+## The rule to check do `case` statements in the code don't contain some
+## expressions. Checked things:
+##
+## * The maximum and minimum amount of `case` statements' branches.
+##
 ## The syntax in a configuration file is::
 ##
-##   [ruleType] ?not? caseStatements
+##   [ruleType] ?not? caseStatements [checkType] [amount]
 ##
 ## * ruleType is the type of rule which will be executed. Proper values are:
 ##   *check*, *search*, *count* and *fix*. For more information about the types of
-##   rules, please refer to the program's documentation. --Insert description
-##   how rules types works with the rule--.
+##   rules, please refer to the program's documentation. Check type will raise
+##   an error if there is a `case` statement which violates any of the checks. Search
+##   type will list all statements which violates any of checks or raise an
+##   error if nothing found. Count type will simply list the amount of the
+##   statements which violates the checks. Fix type will execute the default
+##   shell command set by the program's setting **fixCommand**.
 ## * optional word *not* means negation for the rule. Adding word *not* will
-##   change to inform only about --Insert description how negation affects the
-##   rule--.
+##   change to inform only about the `case` statements which not violate the checks.
+##   Probably useable only with search and count type of rule.
 ## * caseStatements is the name of the rule. It is case-insensitive, thus it can be
-##   set as *casestatements*, *caseStatements* or *--rUlEnAmE--*.
+##   set as *casestatements*, *caseStatements* or *cAsEsTaTeMeNtS*.
+## * checkType is the type of checks to perform on the `case` statements. Proper
+##   values are: *min* and *max*.
+##   Setting it min will check if all `case` statements have at least the selected
+##   amount of branches. Max value will check if the `case` statements have maximum
+##   the selected amount of branches.
+## * amount parameter is required for both types of checks. It is desired amount
+##   of branches for the `case` statements, minimal or maximum, depends on
+##   check's type.
 ##
 ## Disabling the rule
 ## ------------------
 ## It is possible to disable the rule for a selected part of the checked code
-## by using pragma *ruleOff: "caseStatements"* in the element from which the rule
-## should be disabled or in code before it. For example, if the rule should
-## be disabled for procedure `proc main()`, the full declaration of it should
-## be::
+## by using pragma *ruleOff: "caseStatements"* in the code before it. For example,
+## if the rule should be disabled for the statement, the full declaration of it
+## should be::
 ##
-##     proc main () {.ruleOff: "caseStatements".}
+##     {.ruleOff: "caseStatements".}
+##     case a
+##     of 1:
+##       echo a
 ##
-## To enable the rule again, the pragma *ruleOn: "caseStatements"* should be added in
-## the element which should be checked or in code before it. For example, if
-## the rule should be re-enabled for `const a = 1`, the full declaration should
-## be::
+## To enable the rule again, the pragma *ruleOn: "caseStatements"* should be added
+## in the code before it. For example, if the rule should be re-enabled for the
+## statement, the full declaration should be::
 ##
-##     const a {.ruleOn: "caseStatements".} = 1
+##     {.ruleOn: "caseStatements".}
+##     case a
+##     of 1:
+##       echo a
 ##
 ## Examples
 ## --------
 ##
-## --Insert rules examples--
+## 1. Check if all `case` statements have at least 4 branches:
+##
+##     check caseStatements min 4
 
 # Import default rules' modules
 import ../rules
