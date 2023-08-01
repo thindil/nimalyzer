fe10413f5033c8c88fbdd37991553083ec1baff8
thindil <thindil@laeran.pl>
2023-08-01 04:05:15 +0000

    feat: added rule caseStatements
    FossilOrigin-Name: 1eba9077641ad1107d04edd06f4d18e1e71d8b5432bf54edbfcb96b4c78e8410

---
+++ src/rules/casestatements.nim
@@ -0,0 +1,99 @@
+# Copyright © 2023 Bartek thindil Jasicki
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## --Insert here the description of the rule--
+## The syntax in a configuration file is::
+##
+##   [ruleType] ?not? caseStatements
+##
+## * ruleType is the type of rule which will be executed. Proper values are:
+##   *check*, *search*, *count* and *fix*. For more information about the types of
+##   rules, please refer to the program's documentation. --Insert description
+##   how rules types works with the rule--.
+## * optional word *not* means negation for the rule. Adding word *not* will
+##   change to inform only about --Insert description how negation affects the
+##   rule--.
+## * caseStatements is the name of the rule. It is case-insensitive, thus it can be
+##   set as *casestatements*, *caseStatements* or *--rUlEnAmE--*.
+##
+## Disabling the rule
+## ------------------
+## It is possible to disable the rule for a selected part of the checked code
+## by using pragma *ruleOff: "caseStatements"* in the element from which the rule
+## should be disabled or in code before it. For example, if the rule should
+## be disabled for procedure `proc main()`, the full declaration of it should
+## be::
+##
+##     proc main () {.ruleOff: "caseStatements".}
+##
+## To enable the rule again, the pragma *ruleOn: "caseStatements"* should be added in
+## the element which should be checked or in code before it. For example, if
+## the rule should be re-enabled for `const a = 1`, the full declaration should
+## be::
+##
+##     const a {.ruleOn: "caseStatements".} = 1
+##
+## Examples
+## --------
+##
+## --Insert rules examples--
+
+# Import default rules' modules
+import ../rules
+
+ruleConfig(ruleName = "casestatements",
+  ruleFoundMessage = "case statements which can{negation} be upgraded",
+  ruleNotFoundMessage = "case statements which can{negation} be upgraded not found.",
+  rulePositiveMessage = "case statement, line: {params[0]} {params[1]}",
+  ruleNegativeMessage = "case statement, line: {params[0]} {params[1]}",
+  ruleOptions = @[custom, integer],
+  ruleOptionValues = @["min", "max"],
+  ruleMinOptions = 2)
+
+checkRule:
+  initCheck:
+    discard
+  startCheck:
+    let negation: string = (if rule.negation: "'t" else: "")
+  checking:
+    if node.kind == nkCaseStmt:
+      var checkResult: bool = true
+      if rule.options[0].toLowerAscii == "min":
+        if node.len - 1 < rule.options[1].parseInt():
+          checkResult = false
+      elif node.len - 1 > rule.options[1].parseInt():
+        checkResult = false
+      setResult(checkResult = checkResult,
+          positiveMessage = positiveMessage,
+          negativeMessage = negativeMessage, node = node,
+          ruleData = "amount", params = [$node.info.line,
+          "the statement " & (if rule.negation: "doesn't have " else: "has ") &
+          (if rule.options[0].toLowerAscii == "max": "more " else: "less ") &
+              "than " & rule.options[1] & " branches."])
+  endCheck:
+    discard
+
+fixRule:
+  discard
