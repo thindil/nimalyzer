9cb44003b050ce23877118518cd8ed875a5a0675
thindil <thindil@laeran.pl>
2023-08-07 03:22:36 +0000

    tests: continue work on unit test for comments rule
    FossilOrigin-Name: 920eb4b68cb2a76a2589f7d850f7e50f14b6b3db2474daaf06cd00879754639a

--- tests/tcomments/tcomments.nim
+++ tests/tcomments/tcomments.nim
@@ -3,32 +3,29 @@ discard """
   output: '''INFO: Checking the rule's options validation.
 FATAL: The rule comments requires at maximum 2 options, but 3 provided: 'randomoption, anotheroption, thirdoption'.
 INFO: Checking check type of the rule with the invalid code.
-ERROR: Comment at line: 2 doesn't match the pattern '^FIXME'.
+ERROR: Comment at line: 1 doesn't match the pattern '^FIXME'.
 INFO: Checking check type of the rule with the valid code.
 INFO: Checking negative check type of the rule with the valid code.
 ERROR: Comment at line: 1 match the pattern '^FIXME'.
 INFO: Checking negative check type of the rule with the invalid code.
-ERROR: Comment at line: 1 match the pattern '^FIXME'.
 INFO: Checking search type of the rule with the invalid code.
-NOTICE: Comment at line: 1 match the pattern '^FIXME'.
+NOTICE: Comments which match the pattern not found.
 INFO: Checking search type of the rule with the valid code.
-NOTICE: Comment at line: 1 match the pattern '^FIXME'.
+NOTICE: Comments which match the pattern not found.
 INFO: Checking negative search type of the rule with the valid code.
-NOTICE: Comment at line: 2 doesn't match the pattern '^FIXME'.
-NOTICE: Comments which doesn't match the pattern not found.
+NOTICE: Comment at line: 1 doesn't match the pattern '^FIXME'.
 INFO: Checking negative search type of the rule with the invalid code.
-NOTICE: Comment at line: 2 doesn't match the pattern '^FIXME'.
-NOTICE: Comments which doesn't match the pattern not found.
+NOTICE: Comment at line: 1 doesn't match the pattern '^FIXME'.
 INFO: Checking count type of the rule with the invalid code.
-NOTICE: Comments which match the pattern found found: 1
+NOTICE: Comments which match the pattern found found: 0
 INFO: Checking count type of the rule with the valid code.
-NOTICE: Comments which match the pattern found found: 1
+NOTICE: Comments which match the pattern found found: 0
 INFO: Checking negative count type of the rule with the invalid code.
-NOTICE: Comment at line: 2 doesn't match the pattern '^FIXME'.
-NOTICE: Comments which doesn't match the pattern found found: 0
+NOTICE: Comment at line: 1 doesn't match the pattern '^FIXME'.
+NOTICE: Comments which doesn't match the pattern found found: 1
 INFO: Checking negative count type of the rule with the valid code.
-NOTICE: Comment at line: 2 doesn't match the pattern '^FIXME'.
-NOTICE: Comments which doesn't match the pattern found found: 0
+NOTICE: Comment at line: 1 doesn't match the pattern '^FIXME'.
+NOTICE: Comments which doesn't match the pattern found found: 1
 INFO: Checking fix type of the rule.
 ERROR: Comment at line: 2 doesn't match the pattern '^FIXME'.
 INFO: Checking negative fix type of the rule.
