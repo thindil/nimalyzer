fa1336e78220fd6b920cbcd96de75dc324147e62
thindil <thindil@laeran.pl>
2024-02-15 04:52:43 +0000

    tests: redesigned tests for hasPragma rule
    FossilOrigin-Name: 4f4275489777be7bbae78d15502fd0e8a813a5b56a7fae07a21691836ecff1b8

---
+++ tests/haspragma.nim
@@ -0,0 +1,10 @@
+import ../src/rules/haspragma
+import utils/helpers
+
+const
+  validOptions = @["procedures", "raises: []"]
+  invalidOptions = @[]
+  invalidNimCode = "proc MyProc() = discard"
+  validNimCode = "proc MyProc() {.raises: [].} = discard"
+
+runRuleTest(moduleName = "hasPragma rule")
