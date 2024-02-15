7902237f4df943f068c0088b89877ff4dbbf2b48
thindil <thindil@laeran.pl>
2024-02-15 04:49:32 +0000

    tests: redesigned tests for hasDoc rule
    FossilOrigin-Name: f709c87bfead16a26963a9d71ee0c25d782faeea075d688cfea2743296298127

---
+++ tests/hasdoc.nim
@@ -0,0 +1,11 @@
+import ../src/rules/hasdoc
+import utils/helpers
+
+const
+  validOptions: seq[string] = @["all", "tests/utils/doctemplate.txt"]
+  invalidOptions = @["randomoption", "anotheroption", "thirdoption"]
+  invalidNimCode = "var i* = 0"
+  validNimCode = """## Template doc.
+var i* = 0 ## Template doc."""
+
+runRuleTest(moduleName = "hasDoc rule")
