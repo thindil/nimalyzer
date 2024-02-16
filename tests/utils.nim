ef41ce78e809693ad50698568afe326a4a38704c
thindil <thindil@laeran.pl>
2024-02-16 05:03:14 +0000

    tests: redesigned tests for utils module
    FossilOrigin-Name: 272697179a3bf84d61aea838bac9f6c74677f6513685831aacafbb30b3e4afb7

---
+++ tests/utils.nim
@@ -0,0 +1,11 @@
+import ../src/utils
+import utils/helpers
+import unittest2
+
+suite "Unit tests for utils module":
+
+  checkpoint "Initializing the tests"
+  setLogger()
+
+  test "Test showing message":
+    message("Test message")
