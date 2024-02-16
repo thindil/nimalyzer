68ce06b305630777d0fabb8f59b1df9e677c5338
thindil <thindil@laeran.pl>
2024-02-16 04:49:48 +0000

    tests: redesigned tests for namingConv rule
    FossilOrigin-Name: 63ba475d54adcc0f3b602b5c142f588201a46ac92ec400dee4bf413c4da98f13

---
+++ tests/namingconv.nim
@@ -0,0 +1,10 @@
+import ../src/rules/namingconv
+import utils/helpers
+
+const
+  validOptions = @["variables", "[a-z][a-zA-Z0-9_]"]
+  invalidOptions = @[]
+  invalidNimCode = "var IsThe: int = 1"
+  validNimCode = "var isThe: int = 1"
+
+runRuleTest(moduleName = "namingConv rule", disabledChecks = {fixTests})
