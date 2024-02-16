f86bc96c2c26d4b23e5fe2d7a1e854476c50fb0b
thindil <thindil@laeran.pl>
2024-02-16 04:46:44 +0000

    tests: redesigned tests for localHides rule
    FossilOrigin-Name: 3026de87bfa5e0a34ca8aee9cefe53515dea9dfe5fcca05e7dd1c43bcfccb9e4

---
+++ tests/localhides.nim
@@ -0,0 +1,23 @@
+import ../src/rules/localhides
+import utils/helpers
+
+const
+  validOptions: seq[string] = @[]
+  invalidOptions = @["randomoption"]
+  invalidNimCode = """
+var a = 1
+for i in 1 .. 10:
+  var a = 2
+  for j in 2 .. 3:
+    var a = 3"""
+  validNimCode = """
+var a = 1
+for i in 1 .. 10:
+  var locala = 2
+  for j in 2 .. 3:
+    var locallocala = 3"""
+
+# Disable check for invalid code search as it always returns error instead of
+# positive value. Also, disable check for negative fix type of rule as it
+# do nothing.
+runRuleTest(moduleName = "localHides rule", disabledChecks = {invalidSearch, negativeFix})
