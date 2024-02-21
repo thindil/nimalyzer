4e83be8edaee20310fcafec450b28d7c23cab123
thindil <thindil@laeran.pl>
2024-02-21 05:24:15 +0000

    tests: updated tests for assignments rule
    FossilOrigin-Name: 644c0c20cda83c6a82303f65e09b6ba955cf324f718bf3fe2d8b3609d4d62a9c

--- tests/assignments.nim
+++ tests/assignments.nim
@@ -4,9 +4,5 @@ import utils/helpers
 const
   validOptions: seq[string] = @["shorthand"]
   invalidOptions = @["randomoption", "anotheroption"]
-  invalidNimCode = """var i = 1
-i = i + 1"""
-  validNimCode = """var i = 1
-i += 1"""
 
-runRuleTest()
+runRuleTest(files = @["assignments"])
