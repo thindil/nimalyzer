8c1484a7ee28ab54ddf83d62a987767b3e850120
thindil <thindil@laeran.pl>
2024-02-22 04:54:05 +0000

    tests: updated tests for complexity rule
    FossilOrigin-Name: aa3423d3e0bdcf9145d86529a738ed70a40671991e1873b1e2eb5a25efb7e7b1

--- tests/complexity.nim
+++ tests/complexity.nim
@@ -4,9 +4,5 @@ import utils/helpers
 const
   validOptions: seq[string] = @["cyclomatic", "all", "2"]
   invalidOptions = @["randomoption", "anotheroption"]
-  invalidNimCode = """if i == 1 and b == 2:
-  i = i + 1"""
-  validNimCode = """if i == 1:
-  i += 1"""
 
-runRuleTest(disabledChecks = {fixTests})
+runRuleTest(files = @["complexity"], disabledChecks = {fixTests})
