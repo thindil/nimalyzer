9440de841454a0d0a6e8fcfdac6485448b23a2a8
thindil <thindil@laeran.pl>
2024-02-24 04:49:54 +0000

    tests: updated tests for casestatements rule
    FossilOrigin-Name: 0798da7c8bc5c54d80f39e16233864ff2cac1f3f4e653cce0767bd2d04521c4d

--- tests/casestatements.nim
+++ tests/casestatements.nim
@@ -1,8 +1,6 @@
 include ../src/rules/casestatements
 import utils/helpers
 
-const
-  validOptions: seq[string] = @["min", "2"]
-  invalidOptions = @["randomoption", "anotheroption", "andmoreoption"]
-
-runRuleTest(files = @["casestatements"], disabledChecks = {invalidSearch, fixTests})
+runRuleTest(files = @["casestatements"], validOptions = @["min", "2"],
+    invalidOptions = @["randomoption", "anotheroption", "andmoreoption"],
+    disabledChecks = {invalidSearch, fixTests})
