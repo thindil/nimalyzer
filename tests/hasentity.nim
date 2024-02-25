85f4894f813d2d368a35e158d4ae9701ff21ca3c
thindil <thindil@laeran.pl>
2024-02-25 04:47:31 +0000

    tests: updated tests for hasentity rule
    FossilOrigin-Name: 55354a23ec44e0599ed1254569764513cbf62c166064f69b58fa5f3f96d66b9b

--- tests/hasentity.nim
+++ tests/hasentity.nim
@@ -1,8 +1,5 @@
 include ../src/rules/hasentity
 import utils/helpers
 
-const
-  validOptions = @["nkProcDef", "MyProc"]
-  invalidOptions = @[""]
-
-runRuleTest(files = @["hasentity"], disabledChecks = {fixTests})
+runRuleTest(files = @["hasentity"], validOptions = @["nkProcDef", "MyProc"],
+    invalidOptions = @[""], disabledChecks = {fixTests})
