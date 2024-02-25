97e51ceb2f19fc75667695610c95ec4189466a49
thindil <thindil@laeran.pl>
2024-02-25 04:50:10 +0000

    tests: updated tests for localhides rule
    FossilOrigin-Name: 025c3ca667609f13bc92acdd825e8466bf04e0c88d725c4621fc23993052c5d9

--- tests/localhides.nim
+++ tests/localhides.nim
@@ -1,11 +1,8 @@
 include ../src/rules/localhides
 import utils/helpers
 
-const
-  validOptions: seq[string] = @[]
-  invalidOptions = @["randomoption"]
-
 # Disable check for invalid code search as it always returns error instead of
 # positive value. Also, disable check for negative fix type of rule as it
 # do nothing.
-runRuleTest(files = @["localhides"], disabledChecks = {invalidSearch, negativeFix})
+runRuleTest(files = @["localhides"], validOptions = @[], invalidOptions = @[
+    "randomoption"], disabledChecks = {invalidSearch, negativeFix})
