0d995ac27225f76e659ae90fbc43cf9432138370
thindil <thindil@laeran.pl>
2024-02-26 05:13:54 +0000

    tests: updated tests for vardeclared rule
    FossilOrigin-Name: b0e9ce0469a747bb0308b87f98c08b02ef5e6efe1e8dcfbb512d7751fb6424a9

--- tests/vardeclared.nim
+++ tests/vardeclared.nim
@@ -1,8 +1,5 @@
 include ../src/rules/vardeclared
 import utils/helpers
 
-const
-  validOptions = @["type"]
-  invalidOptions = @[]
-
-runRuleTest(files = @["vardeclared"], disabledChecks = {fixTests})
+runRuleTest(files = @["vardeclared"], validOptions = @["type"],
+    invalidOptions = @[], disabledChecks = {fixTests})
