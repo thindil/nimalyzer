9107dfea39226eec8c731789bf051a9687b17fa6
thindil <thindil@laeran.pl>
2024-02-26 05:31:18 +0000

    chore: fixed issues reported by nimalyzer
    FossilOrigin-Name: 2d5a40aefd7404aded63550be1c89a6fccc432ece0f7691e8e40a932d1a6ce2f

--- tests/comments.nim
+++ tests/comments.nim
@@ -1,3 +1,30 @@
+# Copyright © 2024 Bartek Jasicki
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## Provides unit tests for comments rule
+
 import compiler/parser
 include ../src/rules/comments
 import utils/helpers
@@ -17,8 +44,8 @@ suite "Unit tests for comments rule":
   let
     (nimCache, nimConfig) = setNim()
   var
-    validCode = parseString(validNimCode, nimCache, nimConfig)
-    invalidCode = parseString(invalidNimCode, nimCache, nimConfig)
+    validCode = parseString(s = validNimCode, cache = nimCache, config = nimConfig)
+    invalidCode = parseString(s = invalidNimCode, cache = nimCache, config = nimConfig)
     ruleOptions = RuleOptions(parent: true, fileName: "tests/invalid/comments.nim",
         negation: false, ruleType: check, options: validOptions, amount: 0,
         enabled: true, maxResults: Natural.high)
@@ -26,20 +53,20 @@ suite "Unit tests for comments rule":
   test "Checking the rule's options validation.":
     checkpoint "Validate invalid rule's options"
     check:
-      not validateOptions(ruleSettings, invalidOptions)
+      not validateOptions(rule = ruleSettings, options = invalidOptions)
     checkpoint "Validate valid rule's options"
     check:
-      validateOptions(ruleSettings, validOptions)
+      validateOptions(rule = ruleSettings, options = validOptions)
 
   test "Checking check type of the rule":
     checkpoint "Checking the check type of the rule with the invalid code"
-    ruleCheck(invalidCode, invalidCode, ruleOptions)
+    ruleCheck(astNode = invalidCode, parentNode = invalidCode, rule = ruleOptions)
     check:
       ruleOptions.amount == 0
     checkpoint "Checking the check type of the rule with the valid code"
     ruleOptions.parent = true
     ruleOptions.fileName = "tests/valid/comments.nim"
-    ruleCheck(validCode, validCode, ruleOptions)
+    ruleCheck(astNode = validCode, parentNode = validCode, rule = ruleOptions)
     check:
       ruleOptions.amount > 0
 
@@ -48,7 +75,7 @@ suite "Unit tests for comments rule":
     ruleOptions.parent = true
     ruleOptions.negation = true
     ruleOptions.amount = 0
-    ruleCheck(validCode, validCode, ruleOptions)
+    ruleCheck(astNode = validCode, parentNode = validCode, rule = ruleOptions)
     check:
       ruleOptions.amount == 0
     checkpoint "Checking the negative check type of the rule with the invalid code"
