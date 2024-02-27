bc89596f96ed2256166559ec3bdd599af5ad9d0c
thindil <thindil@laeran.pl>
2024-02-27 04:57:39 +0000

    chore: fixed issues reported by nimalyzer
    FossilOrigin-Name: 6c18d195ba47546e144de734af6900b10ce582b0e006459f63ccda85d3441136

--- tests/config.nim
+++ tests/config.nim
@@ -1,4 +1,31 @@
-{.warning[UnusedImport]:off.}
+# Copyright © 2024 Bartek Jasicki
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## Provides unit tests for config module
+
+{.warning[UnusedImport]: off.}
 import ../src/main
 import utils/helpers
 import unittest2
@@ -9,9 +36,9 @@ suite "Unit tests for config module":
   setLogger()
 
   test "Test parsing configuration file":
-    var sections = 0
+    var sections: int = 0
     let (sources, rules, fixCommand, maxReports) = parseConfig(
-        "config/nimalyzer.cfg", sections)
+        configFile = "config/nimalyzer.cfg", sections = sections)
     check:
       sources.len > 0 and rules.len > 0 and fixCommand.len > 0 and sections ==
         1 and maxReports == Natural.high
