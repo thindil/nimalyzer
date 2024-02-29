579f0d6a1a81c3258c1759cf9c1c857e3039b899
thindil <thindil@laeran.pl>
2024-02-29 05:27:35 +0000

    chore: fixed issues reported by nimalyzer
    FossilOrigin-Name: 0be3a71439fa8ccfb1574d44ffc7fcbe2b566a73447e63d991baea9408216f2a

--- tests/rules.nim
+++ tests/rules.nim
@@ -1,3 +1,30 @@
+# Copyright © 2024 Bartek Jasicki
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## Provides unit tests for rules module
+
 import compiler/parser
 include ../src/rules
 import utils/helpers
@@ -12,11 +39,12 @@ suite "Unit tests for rules module":
 
   test "Test showing message":
     checkpoint "Decreasing the value"
-    message("Decrease value", resultValue)
+    message(text = "Decrease value", returnValue = resultValue)
     check:
       resultValue == -1
     checkpoint "Increasing the value"
-    message("Increase value", resultValue, lvlInfo, false)
+    message(text = "Increase value", returnValue = resultValue, level = lvlInfo,
+        decrease = false)
     check:
       resultValue == 0
 
@@ -26,15 +54,16 @@ suite "Unit tests for rules module":
       i.inc
     except OverflowDefect:
       check:
-        errorMessage("Reason: ", getCurrentException()) == 0
+        errorMessage(text = "Reason: ", e = getCurrentException()) == 0
 
   test "Test getting AST nodes to check":
     let
       (nimCache, nimConfig) = setNim()
-      parentNode = parseString("""
+      parentNode = parseString(s = """
       proc hello() =
         var a = 1
         for i in 1..2:
-          echo a""", nimCache, nimConfig)
+          echo a""", cache = nimCache, config = nimConfig)
     check:
-      getNodesToCheck(parentNode, parentNode[0][6]) == flattenStmts(parentNode)
+      getNodesToCheck(parentNode = parentNode, node = parentNode[0][6]) ==
+          flattenStmts(n = parentNode)
