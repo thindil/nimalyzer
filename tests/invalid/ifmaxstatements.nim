4d1de4eb5f1b519611eb4b5488dcae9a398a4d1e
thindil <thindil@laeran.pl>
2024-03-16 04:57:09 +0000

    tests: updated test code for ifstatements rule with max branches
    FossilOrigin-Name: 1a9745d1b14f4d6add2035eabb8eee7e168982fa33cb45ba19e9d3a655fbdab4

--- tests/invalid/ifmaxstatements.nim
+++ tests/invalid/ifmaxstatements.nim
@@ -1,4 +1,4 @@
-# Count: 1
+# Count: 2
 # Negative count: 0
 
 let a = 0
@@ -11,3 +11,14 @@ elif a == 2:
   echo "two"
 else:
   echo "more"
+
+if a > 1:
+  for i in 1 .. 5:
+    if a == 0:
+      echo "zero"
+    elif a == 1:
+      echo "one"
+    elif a == 2:
+      echo "two"
+    else:
+      echo "more"
--- tests/valid/ifmaxstatements.nim
+++ tests/valid/ifmaxstatements.nim
@@ -1,5 +1,5 @@
 # Count: 0
-# Negative count: 1
+# Negative count: 3
 
 let a = 0
 
@@ -7,6 +7,13 @@ if a == 1:
   echo "equal"
 else:
   echo "not equal"
+
+if a > 1:
+  for i in 1 .. 5:
+    if a == 1:
+      echo "equal"
+    else:
+      echo "not equal"
 
 when sizeof(int) == 2:
   echo "running on a 16 bit system!"
