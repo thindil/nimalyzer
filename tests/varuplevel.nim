883b1c3fbf1027b5dd7fad3f8379df664316c177
thindil <thindil@laeran.pl>
2024-03-20 04:39:31 +0000

    tests: enabled search test for invalid code for varuplevel rule
    FossilOrigin-Name: 9677d6fc48b94a0e76e6cd812454e4f3aa90f02752e347e75cd53c4e467ba5a0

--- tests/varuplevel.nim
+++ tests/varuplevel.nim
@@ -29,4 +29,4 @@ include ../src/rules/varuplevel
 import utils/helpers
 
 runRuleTest(files = @["varuplevel"], validOptions = @[], invalidOptions = @[
-    "something"], disabledChecks = {invalidSearch, negativeFix})
+    "something"], disabledChecks = {negativeFix})
