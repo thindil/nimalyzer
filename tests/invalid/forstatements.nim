e93dac30c6d877517316e179ef94bc04d5960fcd
thindil <thindil@laeran.pl>
2024-03-24 05:00:40 +0000

    tests: updated test code for forstatements rule
    FossilOrigin-Name: 531af4c844df8d643f852712edea05c41d72b3fdb0848a7a058b9a11419d8486

--- tests/invalid/forstatements.nim
+++ tests/invalid/forstatements.nim
@@ -1,5 +1,10 @@
-# Count: 1
+# Count: 2
 # Negative count: 0
 
 for i in [1 .. 6]:
   echo i
+  if i > 0:
+    if i == 1:
+      continue
+    for j in [1 .. 6]:
+      echo j
--- tests/valid/forstatements.nim
+++ tests/valid/forstatements.nim
@@ -1,5 +1,10 @@
 # Count: 0
-# Negative count: 1
+# Negative count: 2
 
 for i in [1 .. 6].items:
   echo i
+  if i > 0:
+    if i == 1:
+      continue
+    for j in [1 .. 6].items:
+      echo j
