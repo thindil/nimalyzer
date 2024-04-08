9940e4f5c5943efad1e69c712d96a7d5c66bdb50
thindil <thindil@laeran.pl>
2024-04-08 04:41:43 +0000

    tests: updated test code for forstatements rule
    FossilOrigin-Name: fcfddbc030538d9f7acb3e2ca009ff4d1de2ec21072d5788b236e2f0ab930353

--- tests/invalid/forstatements.nim
+++ tests/invalid/forstatements.nim
@@ -1,10 +1,12 @@
 # Count: 2
 # Negative count: 0
 
-for i in [1 .. 6]:
+const testArray: array[6, int] = [1, 2, 3, 4, 5, 6]
+
+for i in testArray:
   echo i
   if i > 0:
     if i == 1:
       continue
-    for j in [1 .. 6]:
+    for j in testArray:
       echo j
--- tests/valid/forstatements.nim
+++ tests/valid/forstatements.nim
@@ -1,10 +1,12 @@
 # Count: 0
 # Negative count: 2
 
-for i in [1 .. 6].items:
+const testArray: array[6, int] = [1, 2, 3, 4, 5, 6]
+
+for i in testArray.items:
   echo i
   if i > 0:
     if i == 1:
       continue
-    for j in [1 .. 6].items:
+    for j in testArray.items:
       echo j
