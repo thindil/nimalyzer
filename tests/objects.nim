92710a9ea9aee351f03fbdfa93fefaf730870364
thindil <thindil@laeran.pl>
2024-04-11 06:15:47 +0000

    tests: updated tests for objects rule
    FossilOrigin-Name: dd66900639fa299a606458fb617cce6528723a5c22484517ed5bfb90d89b5cf5

--- tests/objects.nim
+++ tests/objects.nim
@@ -28,5 +28,5 @@
 include ../src/rules/objects
 import utils/helpers
 
-runRuleTest(files = @["objects"], validOptions = @["publicfields"],
+runRuleTest(files = @["objects"], validOptions = @["all"],
     invalidOptions = @["randomoption", "anotheroption"])
