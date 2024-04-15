7108a0fe735792981a5ce255a21eedabcddd4286
thindil <thindil@laeran.pl>
2024-04-15 04:09:56 +0000

    refactor: changed types of several variables
    FossilOrigin-Name: fea6c3236e854ef3ba058d0799a6986970fdf7d5b2aeac17c306e0718b5cd2d8

--- src/main.nim
+++ src/main.nim
@@ -72,9 +72,10 @@ proc main() {.raises: [], tags: [ReadIOEffect, WriteIOEffect, RootEffect],
       nimCache: IdentCache = newIdentCache()
       nimConfig: ConfigRef = newConfigRef()
     nimConfig.options.excl(y = optHints)
+    type ExtendedNatural = range[-1 .. Natural.high]
     var
-      resultCode: int = QuitSuccess
-      configSections: int = 0
+      resultCode: int16 = QuitSuccess
+      configSections: ExtendedNatural = 0
       globalShowSummary: bool = false
     # Check source code files with the selected rules
     block checkingCode:
