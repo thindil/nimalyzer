9a5e9f4cb4d62f03a0b094fec91a0c87528677fa
thindil <thindil@laeran.pl>
2024-06-07 04:14:00 +0000

    tests: updated test code for namingconv rule
    FossilOrigin-Name: fabbd5a529c5c9824e5d015e98a896830d7da37e400aa2252b981bc4b4f610b6

--- tests/invalid/namingconv.nim
+++ tests/invalid/namingconv.nim
@@ -1,4 +1,11 @@
 # Count: 0
 # Negative count: 1
 
+type
+  myEnum = enum
+    One
+
 var IsThe: int = 1
+
+proc MyProc() =
+  discard
--- tests/valid/namingconv.nim
+++ tests/valid/namingconv.nim
@@ -1,4 +1,11 @@
 # Count: 1
 # Negative count: 0
 
+type
+  MyEnum = enum
+    one
+
 var isThe: int = 1
+
+proc myProc() =
+  discard
