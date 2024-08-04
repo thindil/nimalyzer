20b60181375e5206a13bdc97e787ac2313d020d4
thindil <thindil@laeran.pl>
2024-08-04 04:27:17 +0000

    tests: updated tests for ranges rule
    FossilOrigin-Name: 36c6b63b267fca3746b0eb4af4e785002886aa0b9edf6681c2f9f1bd492c41b3

--- tests/invalid/ranges.nim
+++ tests/invalid/ranges.nim
@@ -1,5 +1,9 @@
 # Count: 0
-# Negative count: 1
+# Negative count: 2
+
+type a = range[0..5]
+
+let b = "werew..wer"
 
 for i in 1..6:
   echo i
--- tests/valid/ranges.nim
+++ tests/valid/ranges.nim
@@ -1,5 +1,9 @@
 # Count: 1
 # Negative count: 0
 
+type a = range[0 .. 5]
+
+let b = "werew..wer"
+
 for i in 1 .. 6:
   echo i
